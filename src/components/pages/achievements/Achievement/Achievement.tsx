4ab8bd0c0ffa5d35d70f7623aedc63ef4aeb8fc6
Wellington Matheus <w.matheus@outlook.com>
2022-01-07 09:49:00 -0300

    feat(Achievement): create component prop types

---
+++ src/components/pages/achievements/Achievement/Achievement.tsx
@@ -0,0 +1,23 @@
+type Award = {
+  title: string;
+  description: string;
+  imageURL: string;
+};
+
+type AchievementStatus = 'available' | 'unavailable' | 'claimed';
+
+type AchievementRarity = 'common' | 'rare';
+
+type AchievementProps = {
+  title: string;
+  award: Award;
+  status: AchievementStatus;
+  claimCount: number;
+  rarity: AchievementRarity;
+};
+
+function Achievement() {
+  return <div />;
+}
+
+export default Achievement;
