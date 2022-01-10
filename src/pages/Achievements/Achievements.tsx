895b384fca28ee5f44fd3a8422cf2f021bd24a18
Wellington Matheus <w.matheus@outlook.com>
2022-01-10 11:16:54 -0300

    feat(Achievements): use mock data

--- src/pages/Achievements/Achievements.tsx
+++ src/pages/Achievements/Achievements.tsx
@@ -1,5 +1,19 @@
+import { Achievement } from 'components/pages/achievements';
+
+import achievements from './mock';
+
 function Achievements() {
-  return <div />;
+  return (
+    <div>
+      <ul className="flex-row wrap gap-8">
+        {achievements.map(achievement => (
+          <li key={achievement.id}>
+            <Achievement {...achievement} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Achievements;
