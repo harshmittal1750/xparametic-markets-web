0198e5525ab58a8385845313c73637351fe06476
Wellington Matheus <w.matheus@outlook.com>
2022-01-10 11:34:13 -0300

    feat(Achievements): add page title

--- src/pages/Achievements/Achievements.tsx
+++ src/pages/Achievements/Achievements.tsx
@@ -4,7 +4,12 @@ import achievements from './mock';
 
 function Achievements() {
   return (
-    <div>
+    <div className="pm-p-achievements flex-column gap-4">
+      <div className="flex-row justify-space-between align-center padding-bottom-3">
+        <h1 className="pm-p-achievements__title heading semibold">
+          Achievements
+        </h1>
+      </div>
       <ul className="flex-row wrap gap-8">
         {achievements.map(achievement => (
           <li key={achievement.id}>
