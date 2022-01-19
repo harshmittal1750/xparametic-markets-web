bb87d1b25530c8eb877ef624b1e94f90eedb3100
Wellington Matheus <w.matheus@outlook.com>
2022-01-19 09:12:09 -0300

    feat(Environment): group network variables by network id

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,3 +1,6 @@
+import isNull from 'lodash/isNull';
+import isUndefined from 'lodash/isUndefined';
+
 // A environment variable must have the prefix `REACT_APP_` in .env file
 const environmentConfigVariables = [
   'ERC20_CONTRACT_ADDRESS',
@@ -10,8 +13,23 @@ const environmentConfigVariables = [
   'WEB3_PROVIDER'
 ] as const;
 
+const networkConfigVariables = [
+  'PREDICTION_MARKET_CONTRACT_ADDRESS',
+  'REALITIO_ERC20_CONTRACT_ADDRESS',
+  'ERC20_CONTRACT_ADDRESS',
+  'NETWORK_ID',
+  'WEB3_PROVIDER',
+  'WEB3_EVENTS_PROVIDER'
+] as const;
+
+const networkVariableRegularExp = new RegExp(
+  '^REACT_APP_NETWORK_([0-9]+)((_[A-Z0-9]+)*)$'
+);
+
 export type EnvironmentConfigVariable =
   typeof environmentConfigVariables[number];
+
+export type NetworkConfigVariable = typeof networkConfigVariables[number];
 
 export type EnvironmentConfig = {
   [_variable in typeof environmentConfigVariables[number]]: string | undefined;
@@ -31,7 +49,35 @@ function getEnvironmentConfigVariable(
   return process.env[`REACT_APP_${variable}`];
 }
 
+function getNetworksConfigVariables() {
+  const networksVariables = Object.keys(process.env).filter(variable =>
+    variable.match(networkVariableRegularExp)
+  );
+
+  return networksVariables;
+}
+
+function groupNetworksVariablesById(variables: string[]) {
+  const networks = {};
+
+  function addVariableToGroup(variable: string) {
+    const regexMatchResult = variable.match(networkVariableRegularExp);
+    const group = !isNull(regexMatchResult) ? regexMatchResult[1] : undefined;
+
+    if (!isUndefined(group)) {
+      networks[group] = [...(networks[group] || []), variable];
+    }
+  }
+
+  variables.forEach(addVariableToGroup);
+
+  return networks;
+}
+
 function buildEnvironmentConfigObject() {
+  const networkVariables = getNetworksConfigVariables();
+  const networksVariablesById = groupNetworksVariablesById(networkVariables);
+
   return environmentConfigVariables.reduce(
     (acc, variable) => ({
       ...acc,
