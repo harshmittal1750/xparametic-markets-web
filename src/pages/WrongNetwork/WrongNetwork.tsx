b1622c73d471dd0f377ed57fe5856010cefb8bc2
Wellington Matheus <w.matheus@outlook.com>
2022-01-28 23:49:03 -0300

    refactor(WrongNetwork): remove React portal and add Framer animation

--- src/pages/WrongNetwork/WrongNetwork.tsx
+++ src/pages/WrongNetwork/WrongNetwork.tsx
@@ -1,32 +1,44 @@
-import { ModalNotification, Networks, Text } from 'components';
+import { AnimatePresence, motion } from 'framer-motion';
+
+import { Networks, Text } from 'components';
 
 function WrongNetwork() {
   return (
     <div className="pm-wrong-network">
-      <ModalNotification visible>
-        <div className="pm-wrong-network__card">
-          <Text
-            as="h5"
-            scale="heading"
-            fontWeight="bold"
-            color="white"
-            style={{ textAlign: 'center' }}
+      <AnimatePresence>
+        <div className="pm-c-modal-notification">
+          <motion.div
+            layout
+            initial={{ opacity: 0, y: 30, scale: 0.9 }}
+            animate={{ opacity: 1, y: 0, scale: 1 }}
+            exit={{ opacity: 0, y: 20, scale: 0.9 }}
+            role="status"
           >
-            Wrong Network
-          </Text>
-          <span className="spinner--primary" />
-          <Text
-            as="p"
-            scale="body"
-            fontWeight="medium"
-            color="lighter-gray"
-            style={{ textAlign: 'center', paddingBottom: '1rem' }}
-          >
-            Change your MetaMask to one of these available networks
-          </Text>
-          <Networks />
+            <div className="pm-wrong-network__card">
+              <Text
+                as="h5"
+                scale="heading"
+                fontWeight="bold"
+                color="white"
+                style={{ textAlign: 'center' }}
+              >
+                Wrong Network
+              </Text>
+              <span className="spinner--primary" />
+              <Text
+                as="p"
+                scale="body"
+                fontWeight="medium"
+                color="lighter-gray"
+                style={{ textAlign: 'center', paddingBottom: '1rem' }}
+              >
+                Change your MetaMask to one of these available networks
+              </Text>
+              <Networks />
+            </div>
+          </motion.div>
         </div>
-      </ModalNotification>
+      </AnimatePresence>
     </div>
   );
 }
