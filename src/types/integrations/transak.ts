5b2d9a6754ee75fe07f6846011c74fb39f64a645
Wellington Matheus <w.matheus@outlook.com>
2022-02-17 09:54:10 -0300

    feat(Types): add Transak integration

---
+++ src/types/integrations/transak.ts
@@ -0,0 +1,53 @@
+export type TransakUserData = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  mobileNumber: string;
+  dob: string;
+  address: {
+    addressLine1: string;
+    addressLine2: string;
+    city: string;
+    state: string;
+    postCode: string;
+    countryCode: string;
+  };
+};
+
+export type TransakConfig = {
+  apiKey: string;
+  environment?: 'STAGING' | 'PRODUCTION';
+  cryptoCurrencyCode?: string;
+  defaultCryptoCurrency?: string;
+  cryptoCurrencyList?: string;
+  networks?: string;
+  network?: string;
+  walletAddress?: string;
+  walletAddressesData?: string;
+  fiatCurrency?: string;
+  countryCode?: string;
+  fiatAmount?: number;
+  defaultNetwork?: string;
+  defaultFiatAmount?: number;
+  defaultCryptoAmount?: number;
+  paymentMethod?: string;
+  defaultPaymentMethod?: string;
+  disablePaymentMethods?: string;
+  email?: string;
+  userData?: TransakUserData;
+  partnerOrderId?: string;
+  partnerCustomerId?: string;
+  accessToken?: string;
+  hostURL?: string;
+  redirectURL?: string;
+  disableWalletAddressForm?: boolean;
+  isAutoFillUserData?: boolean;
+  themeColor?: string;
+  widgetHeight?: string;
+  widgetWidth?: string;
+  hideMenu?: boolean;
+  hideExchangeScreen?: boolean;
+  isDisableCrypto?: boolean;
+  isFeeCalculationHidden?: boolean;
+  exchangeScreenTitle?: string;
+};
