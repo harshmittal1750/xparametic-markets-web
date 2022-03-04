652754feb3c8d2c17af12c44d83a8025f5462e91
Wellington Matheus <w.matheus@outlook.com>
2022-03-04 11:39:32 -0300

    Revert "feat(Achievements): use mock data"
    This reverts commit 895b384fca28ee5f44fd3a8422cf2f021bd24a18.

--- src/pages/Achievements/Achievements.tsx
+++ src/pages/Achievements/Achievements.tsx
@@ -1,19 +1,5 @@
-import { Achievement } from 'components/pages/achievements';
-
-import achievements from './mock';
-
 function Achievements() {
-  return (
-    <div>
-      <ul className="flex-row wrap gap-8">
-        {achievements.map(achievement => (
-          <li key={achievement.id}>
-            <Achievement {...achievement} />
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <div />;
 }
 
 export default Achievements;
