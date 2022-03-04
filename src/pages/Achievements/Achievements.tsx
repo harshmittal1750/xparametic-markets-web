bde2847bc3eb5e453863c6a1daf8b947d4a23661
Wellington Matheus <w.matheus@outlook.com>
2022-03-04 11:39:12 -0300

    Revert "feat(Achievements): add page title"
    This reverts commit 0198e5525ab58a8385845313c73637351fe06476.

--- src/pages/Achievements/Achievements.tsx
+++ src/pages/Achievements/Achievements.tsx
@@ -4,12 +4,7 @@ import achievements from './mock';
 
 function Achievements() {
   return (
-    <div className="pm-p-achievements flex-column gap-4">
-      <div className="flex-row justify-space-between align-center padding-bottom-3">
-        <h1 className="pm-p-achievements__title heading semibold">
-          Achievements
-        </h1>
-      </div>
+    <div>
       <ul className="flex-row wrap gap-8">
         {achievements.map(achievement => (
           <li key={achievement.id}>
