ea55c1615f564ad3d6ac8fbefe7b4fffe5159d87
Wellington Matheus <w.matheus@outlook.com>
2022-03-04 11:38:49 -0300

    Revert "feat(Achievements): create achievement list"
    This reverts commit 3836e8a72ff7dc5a080044b57c505375ef778bd6.

--- src/pages/Achievements/Achievements.tsx
+++ src/pages/Achievements/Achievements.tsx
@@ -10,7 +10,7 @@ function Achievements() {
           Achievements
         </h1>
       </div>
-      <ul className="pm-p-achievements__list">
+      <ul className="flex-row wrap gap-8">
         {achievements.map(achievement => (
           <li key={achievement.id}>
             <Achievement {...achievement} />
