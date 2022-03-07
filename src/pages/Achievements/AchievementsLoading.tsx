c93eac5e4619c2584ca15a5b5082a3fd75cc77ff
Wellington Matheus <w.matheus@outlook.com>
2022-03-07 10:00:32 -0300

    feat(Achievements): create loading state

---
+++ src/pages/Achievements/AchievementsLoading.tsx
@@ -0,0 +1,9 @@
+function AchievementsLoading() {
+  return (
+    <div className="flex-column justify-center align-center width-full height-full padding-top-9">
+      <span className="spinner--primary" />
+    </div>
+  );
+}
+
+export default AchievementsLoading;
