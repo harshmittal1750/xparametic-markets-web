0eaba514e15312cce59f5bfddd105f51fb195b39
Wellington Matheus <w.matheus@outlook.com>
2022-05-10 11:09:44 -0300

    feat(LeaderboardIcons): export place icons

--- src/assets/icons/pages/leaderboard/index.tsx
+++ src/assets/icons/pages/leaderboard/index.tsx
@@ -1,5 +1,15 @@
+import FirstPlaceIcon from './FirstPlaceIcon';
 import RankDownIcon from './RankDownIcon';
 import RankStableIcon from './RankStableIcon';
 import RankUpIcon from './RankUpIcon';
+import SecondPlaceIcon from './SecondPlaceIcon';
+import ThirdPlaceIcon from './ThirdPlaceIcon';
 
-export { RankDownIcon, RankStableIcon, RankUpIcon };
+export {
+  FirstPlaceIcon,
+  RankDownIcon,
+  RankStableIcon,
+  RankUpIcon,
+  SecondPlaceIcon,
+  ThirdPlaceIcon
+};
