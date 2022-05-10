bd0867f8273e1bd467ab72f929b014d73ee3c70c
Wellington Matheus <w.matheus@outlook.com>
2022-05-10 10:52:09 -0300

    feat(LeaderboardIcons): export rank icons

---
+++ src/assets/icons/pages/leaderboard/index.tsx
@@ -0,0 +1,5 @@
+import RankDownIcon from './RankDownIcon';
+import RankStableIcon from './RankStableIcon';
+import RankUpIcon from './RankUpIcon';
+
+export { RankDownIcon, RankStableIcon, RankUpIcon };
