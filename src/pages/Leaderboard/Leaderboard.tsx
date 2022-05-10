817cb00b1e382cd36dfc0405050164faafc0423b
Wellington Matheus <w.matheus@outlook.com>
2022-05-10 10:41:26 -0300

    feat(Leaderboard): create tabs

--- src/pages/Leaderboard/Leaderboard.tsx
+++ src/pages/Leaderboard/Leaderboard.tsx
@@ -1,5 +1,17 @@
+import { Tabs } from 'components';
+
 function Leaderboard() {
-  return <div>Leaderboard</div>;
+  return (
+    <div className="flex-column justify-start align-start gap-6 width-full">
+      <h1 className="heading semibold text-1">Leaderboard</h1>
+      <Tabs direction="row" defaultActiveId="volume">
+        <Tabs.TabPane tab="Volume" id="volume" />
+        <Tabs.TabPane tab="Markets created" id="marketsCreated" />
+        <Tabs.TabPane tab="Won predictions" id="wonPredictions" />
+        <Tabs.TabPane tab="Liquidity added" id="liquidityAdded" />
+      </Tabs>
+    </div>
+  );
 }
 
 export default Leaderboard;
