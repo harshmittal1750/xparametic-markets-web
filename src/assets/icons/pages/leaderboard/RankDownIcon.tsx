2b4aff7e51c4dc67defabb601e4339347940cb8d
Wellington Matheus <w.matheus@outlook.com>
2022-06-06 10:18:00 -0300

    feat(RankDownIcon): add theme based background color

--- src/assets/icons/pages/leaderboard/RankDownIcon.tsx
+++ src/assets/icons/pages/leaderboard/RankDownIcon.tsx
@@ -1,6 +1,11 @@
 import { SVGProps, memo } from 'react';
 
+import { useTheme } from 'hooks';
+
 function RankDownIcon(props: SVGProps<SVGSVGElement>) {
+  const { theme } = useTheme();
+
+  const backgroundColor = theme === 'dark' ? '#FFFFFF' : '#000000';
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -14,7 +19,7 @@ function RankDownIcon(props: SVGProps<SVGSVGElement>) {
         width="20"
         height="20"
         y="0"
-        fill="#fff"
+        fill={backgroundColor}
         fillOpacity="0.1"
         rx="10"
       />
@@ -24,11 +29,6 @@ function RankDownIcon(props: SVGProps<SVGSVGElement>) {
           d="M10 14.75c.204 0 .396-.1.512-.267l4.375-6.25a.625.625 0 00-.512-.983h-8.75a.624.624 0 00-.512.983l4.375 6.25a.622.622 0 00.511.267h.002c-.001 0-.001 0 0 0z"
         />
       </g>
-      <defs>
-        <clipPath id="clip0_2442_57798">
-          <path fill="#fff" d="M0 0H10V10H0z" transform="rotate(-180 7.5 8)" />
-        </clipPath>
-      </defs>
     </svg>
   );
 }
