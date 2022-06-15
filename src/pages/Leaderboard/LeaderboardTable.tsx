9d890ef9b92f20d98d70d39810e2ee31f16d90e3
Wellington Matheus <w.matheus@outlook.com>
2022-06-15 10:36:35 -0300

    feat(LeaderboardTable): add width and min-width

--- src/pages/Leaderboard/LeaderboardTable.tsx
+++ src/pages/Leaderboard/LeaderboardTable.tsx
@@ -34,8 +34,16 @@ function LeaderboardTable({
     [ticker, loggedInUser, rows, sortBy]
   );
 
+  const tableMinWidth = loggedInUser ? 1200 : 950;
+
   return (
-    <Table columns={columns} rows={preparedRows} isLoadingData={isLoading} />
+    <Table
+      height={700}
+      minWidth={tableMinWidth}
+      columns={columns}
+      rows={preparedRows}
+      isLoadingData={isLoading}
+    />
   );
 }
 
