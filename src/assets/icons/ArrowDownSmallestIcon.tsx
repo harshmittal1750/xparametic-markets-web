d8b1248d028aa10b5199982f133cca05fc009fe1
Wellington Matheus <w.matheus@outlook.com>
2022-06-19 19:42:30 -0300

    feat(ArrowDownSmallestIcon): add SVG props

--- src/assets/icons/ArrowDownSmallestIcon.tsx
+++ src/assets/icons/ArrowDownSmallestIcon.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import { SVGProps, memo } from 'react';
 
-function ArrowDownSmallestIcon() {
+function ArrowDownSmallestIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       width="12"
@@ -8,10 +8,11 @@ function ArrowDownSmallestIcon() {
       viewBox="0 0 12 13"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      {...props}
     >
       <path d="M11.025 7.475L9.97499 6.425L6.74999 9.65V0.5H5.24999V9.65L2.02499 6.425L0.974991 7.475L5.99999 12.5L11.025 7.475Z" />
     </svg>
   );
 }
 
-export default React.memo(ArrowDownSmallestIcon);
+export default memo(ArrowDownSmallestIcon);
