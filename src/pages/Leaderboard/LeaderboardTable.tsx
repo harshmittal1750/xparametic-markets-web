2b8d08b24bfffe5d26aa1af132a75b627d719665
Wellington Matheus <w.matheus@outlook.com>
2022-06-20 14:31:09 -0300

    refactor(LeaderboardTable): remove min-width prop

--- src/pages/Leaderboard/LeaderboardTable.tsx
+++ src/pages/Leaderboard/LeaderboardTable.tsx
@@ -51,12 +51,9 @@ function LeaderboardTable({
     return undefined;
   }, [loggedInUser, preparedRows]);
 
-  const tableMinWidth = loggedInUser ? 1200 : 950;
-
   return (
     <Table
       height={700}
-      minWidth={tableMinWidth}
       columns={columns}
       rows={preparedRows}
       isLoadingData={isLoading}
