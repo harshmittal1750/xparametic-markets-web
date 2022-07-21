a3da0cf0cae999007e76195ed7307ddc79ff288e
Wellington Matheus <w.matheus@outlook.com>
2022-07-21 09:31:56 -0300

    feat(Profile): create your stats types

---
+++ src/pages/Profile/types.tsx
@@ -0,0 +1,9 @@
+import { TableMiniColumn, TableMiniRow } from 'components/new/TableMini';
+
+// Leaderboard ranks
+export type LeaderboardRanksColumn = TableMiniColumn;
+export type LeaderboardRanksRow = TableMiniRow;
+
+// Prediction statistics
+export type PredictionStatisticsColumn = TableMiniColumn;
+export type PredictionStatisticsRow = TableMiniRow;
