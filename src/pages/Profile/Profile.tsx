6dd9e31bd6dd380e886c9f59cd066acc8af2295b
Wellington Matheus <w.matheus@outlook.com>
2022-08-04 20:07:58 -0300

    feat(Profile): get address from route params

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,12 +1,20 @@
+import { useParams } from 'react-router-dom';
+
 import ProfileAchievements from './ProfileAchievements';
 import ProfileActivities from './ProfileActivities';
 import ProfileSummary from './ProfileSummary';
 import ProfileYourStats from './ProfileYourStats';
 
+type ProfileUrlParams = {
+  address: string;
+};
+
 function Profile() {
+  const { address } = useParams<ProfileUrlParams>();
+
   return (
     <div className="flex-column gap-5">
-      <ProfileSummary />
+      <ProfileSummary address={address} />
       <ProfileYourStats />
       <div className="flex-row gap-5 margin-top-6">
         <ProfileAchievements />
