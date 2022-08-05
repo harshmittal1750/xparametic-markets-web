473927272d1e5b8e78fbfb28083b0ca2cf8a29b5
Wellington Matheus <w.matheus@outlook.com>
2022-08-04 22:48:42 -0300

    feat(ProfileTypes): create LeaderboardRanks

--- src/pages/Profile/types.tsx
+++ src/pages/Profile/types.tsx
@@ -3,6 +3,12 @@ import { TableMiniColumn, TableMiniRow } from 'components/new/TableMini';
 // Leaderboard ranks
 export type LeaderboardRanksColumn = TableMiniColumn;
 export type LeaderboardRanksRow = TableMiniRow;
+export type LeaderboardRanks = {
+  rankByVolume: number;
+  rankByMarketsCreated: number;
+  rankByWonPredictions: number;
+  rankByLiquidityAdded: number;
+};
 
 // Prediction statistics
 export type PredictionStatisticsColumn = TableMiniColumn;
