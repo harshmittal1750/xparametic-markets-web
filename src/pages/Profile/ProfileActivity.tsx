d6b1ab34a20297a40b2befe56a68e3d387be6222
Wellington Matheus <w.matheus@outlook.com>
2022-08-05 04:31:59 -0300

    feat(ProfileActivity): remove mock data

--- src/pages/Profile/ProfileActivity.tsx
+++ src/pages/Profile/ProfileActivity.tsx
@@ -1,52 +1,37 @@
-import nfl from './mock/images/nfl.png';
-import worldCup from './mock/images/world_cup.png';
-
-const mock = {
-  won: {
-    title: 'You won a prediction on the market',
-    marketTitle: 'Will Germany finish first in Group E of FIFA World Cup 2022?',
-    image: worldCup,
-    timestamp: '2 weeks ago',
-    bg: '2'
-  },
-  lost: {
-    title: 'You lost a prediction on the market',
-    marketTitle:
-      'Will Los Angeles Rams win against Buffalo Bills in the NFL match on 9th September 2022?',
-    image: nfl,
-    timestamp: '2 weeks ago',
-    bg: '3'
-  }
-};
+import { relativeTimeFromNow } from 'helpers/date';
+import { FeedActivity } from 'types/portfolio';
 
 type ProfileActivityProps = {
-  result: 'won' | 'lost';
+  activity: FeedActivity;
+  backgroundColor: '2' | '3';
 };
 
-function ProfileActivity({ result }: ProfileActivityProps) {
-  const mockByResult = mock[result];
+function ProfileActivity({ activity, backgroundColor }: ProfileActivityProps) {
+  const { accentColor, imageUrl, marketTitle, actionTitle, timestamp } =
+    activity;
+
   return (
     <div
-      className={`pm-c-activity flex-row align-center padding-y-7 padding-x-6 margin-right-4 gap-6 width-full bg-${mockByResult.bg}`}
+      className={`pm-c-activity flex-row align-center padding-y-7 padding-x-6 margin-right-4 gap-6 width-full bg-${backgroundColor}`}
     >
-      <div className={`pm-c-activity__image--${result} border-radius-50`}>
+      <div className={`pm-c-activity__image--${accentColor} border-radius-50`}>
         <img
           className="border-radius-50"
-          src={mockByResult.image}
-          alt={mockByResult.title}
+          src={imageUrl}
+          alt={marketTitle}
           width={64}
           height={64}
         />
       </div>
       <div className="flex-column gap-3">
         <h1
-          className={`pm-c-activity__title--${result} tiny-uppercase semibold`}
+          className={`pm-c-activity__title--${accentColor} tiny-uppercase semibold`}
         >
-          {mockByResult.title}
+          {actionTitle}
         </h1>
-        <p className="body font-semibold text-1">{mockByResult.marketTitle}</p>
+        <p className="body font-semibold text-1">{marketTitle}</p>
         <span className="tiny-uppercase semibold text-3">
-          {mockByResult.timestamp}
+          {relativeTimeFromNow(timestamp * 1000)}
         </span>
       </div>
     </div>
