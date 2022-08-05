543d3dd168c242212f0c6f30cc02a3f88c803985
Wellington Matheus <w.matheus@outlook.com>
2022-08-05 03:02:13 -0300

    feat(ProfileAchievement): remove mock data

--- src/pages/Profile/ProfileAchievement.tsx
+++ src/pages/Profile/ProfileAchievement.tsx
@@ -1,51 +1,48 @@
+import { LeaderboardAchievement } from 'types/leaderboard';
+
 import { MedalIcon } from 'assets/icons';
 
 import { Divider } from 'components';
 
-const mock = {
-  title: 'Count Fakula Polky',
-  actionTitle: 'First Prediction Award',
-  description:
-    'Behold the magnificent crypto beast… wait… but this Vlad is not that into blood and he’s not that good when it comes to dealing cryptos. But who can blame him? Fake it until you make it.',
-  imageUrl:
-    'https://ipfs.io/ipfs/QmYVsPWS9BvJrTZMZrnpw3a7Mgkr1y4nq6kGACiucqSuQH',
-  tokenCount: 1485,
-  rarity: 'common'
-};
-
 type ProfileAchievementProps = {
+  achievement: LeaderboardAchievement;
   backgroundColor: '2' | '3';
 };
 
-function ProfileAchievement({ backgroundColor }: ProfileAchievementProps) {
+function ProfileAchievement({
+  achievement,
+  backgroundColor
+}: ProfileAchievementProps) {
+  const { image, name, actionTitle, rarity } = achievement;
+
   return (
     <div
       className={`flex-row align-center padding-6 margin-right-4 width-full gap-6 bg-${backgroundColor}`}
     >
-      <img src={mock.imageUrl} alt={mock.title} width={104} height={104} />
+      <img src={image} alt={name} width={104} height={104} />
       <div className="flex-column gap-3 width-full">
         <h4 className="pm-c-achievement__award-title heading-large bold">
-          {mock.title}
+          {name}
         </h4>
-        <p className="pm-c-achievement__award-description caption medium">
+        {/* <p className="pm-c-achievement__award-description caption medium">
           {mock.description}
-        </p>
+        </p> */}
         <div className="flex-row justify-space-between align-center gap-5">
           <div className="flex-row justify-start align-center gap-3">
             <MedalIcon className="pm-c-achievement__medal-icon--unlocked" />
             <h1 className="pm-c-achievement__title--unlocked tiny-uppercase semibold">
-              {mock.actionTitle}
+              {actionTitle}
             </h1>
           </div>
           <div className="flex-row align-center gap-3">
-            <span className="pm-c-achievement__claim-count tiny-uppercase semibold">
+            {/* <span className="pm-c-achievement__claim-count tiny-uppercase semibold">
               {`${mock.tokenCount} claimed`}
             </span>
-            <Divider variant="circle" />
+            <Divider variant="circle" /> */}
             <span
-              className={`pm-c-achievement__rarity--${mock.rarity} tiny-uppercase semibold`}
+              className={`pm-c-achievement__rarity--${rarity} tiny-uppercase semibold`}
             >
-              {mock.rarity}
+              {rarity}
             </span>
           </div>
         </div>
