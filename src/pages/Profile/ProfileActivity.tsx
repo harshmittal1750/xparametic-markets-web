cc0376ba8958ee09b8e6c87332cf9838099969a8
Wellington Matheus <w.matheus@outlook.com>
2022-08-08 12:03:14 -0300

    refactor(ProfileActivity): create mobile layout breakpoints

--- src/pages/Profile/ProfileActivity.tsx
+++ src/pages/Profile/ProfileActivity.tsx
@@ -1,6 +1,8 @@
 import { relativeTimeFromNow } from 'helpers/date';
 import { FeedActivity } from 'types/portfolio';
 
+import { Text } from 'components/new';
+
 type ProfileActivityProps = {
   activity: FeedActivity;
   backgroundColor: '2' | '3';
@@ -18,7 +20,7 @@ function ProfileActivity({ activity, backgroundColor }: ProfileActivityProps) {
 
   return (
     <a
-      className={`pm-c-activity flex-row align-center padding-y-7 padding-x-6 margin-right-4 gap-6 width-full bg-${backgroundColor}`}
+      className={`pm-c-activity bg-${backgroundColor}`}
       href={`/markets/${marketSlug}?m=f`}
     >
       <div className={`pm-c-activity__image--${accentColor} border-radius-50`}>
@@ -30,16 +32,34 @@ function ProfileActivity({ activity, backgroundColor }: ProfileActivityProps) {
           height={64}
         />
       </div>
-      <div className="flex-column gap-3">
-        <h1
-          className={`pm-c-activity__title--${accentColor} tiny-uppercase semibold`}
+      <div className="flex-column gap-3 width-full">
+        <Text
+          as="h1"
+          fontSize="body-4"
+          fontWeight="semibold"
+          transform="uppercase"
+          className={`pm-c-activity__action-title--${accentColor}`}
         >
           {actionTitle}
-        </h1>
-        <p className="body font-semibold text-1">{marketTitle}</p>
-        <span className="tiny-uppercase semibold text-3">
+        </Text>
+        <Text
+          className="pm-c-activity__title"
+          as="p"
+          fontSize="body-1"
+          fontWeight="semibold"
+          color="1"
+        >
+          {marketTitle}
+        </Text>
+        <Text
+          as="span"
+          fontSize="body-4"
+          fontWeight="semibold"
+          transform="uppercase"
+          color="3"
+        >
           {relativeTimeFromNow(timestamp * 1000)}
-        </span>
+        </Text>
       </div>
     </a>
   );
