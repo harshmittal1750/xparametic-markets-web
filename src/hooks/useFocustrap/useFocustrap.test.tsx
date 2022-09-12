0d02350e798cf3a02d3e6b940af6ce963c472b40
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-09-12 12:22:44 -0300

    test: add for `useFocustrap`

---
+++ src/hooks/useFocustrap/useFocustrap.test.tsx
@@ -0,0 +1,51 @@
+import { useRef } from 'react';
+
+import { waitFor } from '@testing-library/dom';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { getFocusTrapId } from 'helpers';
+
+import useFocustrap, { Trap } from './useFocustrap';
+
+function Component() {
+  const ref = useRef(null);
+
+  useFocustrap(ref);
+
+  return (
+    <div ref={ref}>
+      <button type="button">button</button>
+    </div>
+  );
+}
+
+describe('useFocustrap', () => {
+  it('should insert the focus trappers elements correctly', () => {
+    render(<Component />);
+
+    Object.values(Trap).forEach(trap =>
+      expect(screen.getByTestId(getFocusTrapId(trap))).toBeTruthy()
+    );
+  });
+  it('should focus the start trapper element and trap it inside the container correctly', async () => {
+    render(<Component />);
+
+    const button = screen.getByText('button');
+
+    expect(screen.getByTestId(getFocusTrapId(Trap.START))).toHaveFocus();
+    userEvent.tab();
+    expect(button).toHaveFocus();
+    userEvent.tab();
+    await waitFor(() => expect(button).toHaveFocus());
+  });
+  it('should focus the previous element on unmount back correctly', async () => {
+    const { unmount } = render(<Component />);
+
+    expect(screen.getByTestId(getFocusTrapId(Trap.START))).toHaveFocus();
+    unmount();
+    Object.values(Trap).forEach(trap =>
+      expect(screen.queryByTestId(getFocusTrapId(trap))).not.toBeInTheDocument()
+    );
+    expect(document.body).toHaveFocus();
+  });
+});
