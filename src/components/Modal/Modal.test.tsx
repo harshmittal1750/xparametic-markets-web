05d85e437250a7e3f98ddd850c9d7c31fc887e7e
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-09-14 15:34:29 -0300

    test: add missed ones for `Modal`

--- src/components/Modal/Modal.test.tsx
+++ src/components/Modal/Modal.test.tsx
@@ -1,51 +1,115 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { getFocusTrapId } from 'helpers';
+
+import ModalFooter from 'components/ModalFooter';
+import ModalHeader from 'components/ModalHeader';
+import ModalHeaderTitle from 'components/ModalHeaderTitle';
+import ModalSection from 'components/ModalSection';
+import ModalSectionText from 'components/ModalSectionText';
 
 import Modal from './Modal';
 
 const defaultProps = {
-  show: false,
+  show: true,
   onHide: jest.fn(),
   name: 'test'
 };
-const defaultData = {
-  title: 'Modal Title',
-  description: 'Modal description text',
-  labelAction: 'Modal action'
-};
-function renderHelper(props = defaultProps, data = defaultData) {
-  return render(
-    <Modal {...props}>
-      <Modal.Header>
-        <Modal.HeaderTitle>{data.title}</Modal.HeaderTitle>
-      </Modal.Header>
-      <Modal.Section>
-        <Modal.SectionText>{data.description}</Modal.SectionText>
-      </Modal.Section>
-      <Modal.Footer>
-        <button type="button">{data.labelAction}</button>
-      </Modal.Footer>
+
+enum A11y {
+  Title,
+  Description
+}
+
+function renderModal() {
+  const result = render(
+    <Modal {...defaultProps}>
+      <ModalHeader>
+        <ModalHeaderTitle>{A11y.Title}</ModalHeaderTitle>
+      </ModalHeader>
+      <ModalSection>
+        <ModalSectionText>{A11y.Description}</ModalSectionText>
+      </ModalSection>
+      <ModalFooter>
+        <button type="button">action</button>
+      </ModalFooter>
     </Modal>
   );
+  const els = {
+    root: screen.getByRole('dialog', {
+      name: `${A11y.Title}`
+    }),
+    overlay: screen.getByRole('presentation'),
+    hide: screen.getByRole('button', {
+      name: 'Hide'
+    })
+  };
+
+  return {
+    els,
+    ...result
+  };
 }
 
 describe('Modal', () => {
-  it('should not render', () => {
-    renderHelper();
+  // todo: renders its content throught usePortal
+  it('locks body overflow', () => {
+    renderModal();
 
-    expect(screen.queryByRole('dialog')).not.toBeInTheDocument();
+    expect(document.body).toHaveStyle({
+      overflow: 'hidden'
+    });
   });
-  it('should render correctly', () => {
-    renderHelper({ ...defaultProps, show: true });
+  it('renders sr title', () => {
+    const { els } = renderModal();
 
-    expect(screen.getByRole('dialog')).toBeInTheDocument();
-    expect(screen.getByText(defaultData.title)).toBeInTheDocument();
-    expect(screen.getByText(defaultData.description)).toBeInTheDocument();
-    expect(screen.getByText(defaultData.labelAction)).toBeInTheDocument();
+    expect(els.root).toBeInTheDocument();
+    expect(
+      screen.getByRole('heading', {
+        level: 2,
+        name: `${A11y.Title}`
+      })
+    );
   });
-  it('should call onHide correctly', () => {
-    renderHelper({ ...defaultProps, show: true });
+  it('animates its overlay and root element', () => {
+    const { els } = renderModal();
 
-    fireEvent.click(screen.getByRole('button', { name: /hide/i }));
+    expect(els.overlay).toHaveStyle({
+      opacity: 0
+    });
+    expect(els.root).toHaveStyle({
+      transform: 'translateY(16px) translateZ(0)'
+    });
+  });
+  it('calls [onHide] throught useClickaway', async () => {
+    const { els } = renderModal();
+
+    fireEvent.focusOut(els.root);
+    await waitFor(() => expect(defaultProps.onHide).toHaveBeenCalled());
+  });
+  it('calls [onHide] throught Escape keydown', () => {
+    const { els } = renderModal();
+
+    fireEvent.keyDown(els.overlay, { key: 'Escape' });
     expect(defaultProps.onHide).toHaveBeenCalled();
+  });
+  // todo: positions focus trapper elements in order
+  it('traps focus on focusable elements inside it throught useFocustrap', async () => {
+    const { els } = renderModal();
+
+    expect(screen.getByTestId(getFocusTrapId('start'))).toHaveFocus();
+
+    userEvent.tab();
+    expect(els.hide).toHaveFocus();
+
+    userEvent.tab();
+    expect(
+      screen.getByRole('button', {
+        name: 'action'
+      })
+    ).toHaveFocus();
+
+    userEvent.tab();
+    await waitFor(() => expect(els.hide).toHaveFocus());
   });
 });
