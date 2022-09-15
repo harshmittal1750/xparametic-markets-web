702772aa5c8438bacf63df8c0f374d91c36b41b4
Wellington Matheus <w.matheus@outlook.com>
2022-09-15 06:50:56 -0300

    feat(VirtualizedList): create `atBottom` state and callback

--- src/components/VirtualizedList/VirtualizedList.tsx
+++ src/components/VirtualizedList/VirtualizedList.tsx
@@ -1,17 +1,33 @@
-import { ReactNode } from 'react';
+import { ReactNode, useState, useEffect } from 'react';
 import { Virtuoso } from 'react-virtuoso';
 
 type VirtualizedListProps<T> = {
   height: number | string;
   data?: T[];
   itemContent: (_index: number, _item: T) => ReactNode;
+  atBottom?: (_atBottom: boolean) => void;
 };
 
 function VirtualizedList<T>({
   height,
-  data,
-  itemContent
+  data = [],
+  itemContent,
+  atBottom
 }: VirtualizedListProps<T>) {
+  const [visibleRange, setVisibleRange] = useState({
+    startIndex: 0,
+    endIndex: 0
+  });
+
+  const lastIndex = data.length - 1;
+
+  const isAtBottom =
+    visibleRange.startIndex <= lastIndex && lastIndex <= visibleRange.endIndex;
+
+  useEffect(() => {
+    atBottom?.(isAtBottom);
+  }, [atBottom, isAtBottom]);
+
   return (
     <Virtuoso
       style={{
@@ -19,6 +35,7 @@ function VirtualizedList<T>({
       }}
       data={data}
       itemContent={itemContent}
+      rangeChanged={setVisibleRange}
     />
   );
 }
