b33d702fa050697471ea0c77e345fe8f8af76fac
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-09-15 22:13:32 -0300

    test: add missed ones for `ModalHeaderTitle`

--- src/components/ModalHeaderTitle/ModalHeaderTitle.test.tsx
+++ src/components/ModalHeaderTitle/ModalHeaderTitle.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import cn from 'classnames';
 
 import ModalHeaderTitle from './ModalHeaderTitle';
 
@@ -13,6 +14,24 @@ describe('ModalHeaderTitle', () => {
       })
     ).toBeInTheDocument();
   });
-  // todo: classname
-  // todo: id
+  it('accepts extra [className]', () => {
+    render(
+      <ModalHeaderTitle
+        className={cn('class', {
+          name: true
+        })}
+      >
+        header
+      </ModalHeaderTitle>
+    );
+
+    expect(screen.getByText('header')).toHaveClass(
+      'pm-c-modal__header-title class name'
+    );
+  });
+  it('accepts extra props', () => {
+    render(<ModalHeaderTitle id="title">title</ModalHeaderTitle>);
+
+    expect(screen.getByText('title')).toHaveAttribute('id', 'title');
+  });
 });
