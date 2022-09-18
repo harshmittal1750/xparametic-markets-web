72b28d6c268c5a8b1531b8d372f55f475c038e61
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-09-18 16:15:29 -0300

    feat: prefer using style modules for `ModalHeader`

--- src/components/ModalHeader/ModalHeader.tsx
+++ src/components/ModalHeader/ModalHeader.tsx
@@ -1,7 +1,10 @@
 import cn from 'classnames';
 
+import ModalHeaderStyles from './ModalHeader.module.scss';
 import type { ModalHeaderProps } from './ModalHeader.type';
 
 export default function ModalHeader({ className, ...props }: ModalHeaderProps) {
-  return <header className={cn('pm-c-modal__header', className)} {...props} />;
+  return (
+    <header className={cn(ModalHeaderStyles.root, className)} {...props} />
+  );
 }
