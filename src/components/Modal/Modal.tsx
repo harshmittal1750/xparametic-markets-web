0c204eb4429b93ab8836685e008001e7ace5c500
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-09-19 12:56:30 -0300

    feat: avoid memory leak and type fixes

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -7,7 +7,13 @@ import { RemoveOutlinedIcon } from 'assets/icons';
 
 import { Button } from 'components/Button';
 
-import { useClickaway, usePortal, usePrevious, useFocustrap } from 'hooks';
+import {
+  useClickaway,
+  usePortal,
+  usePrevious,
+  useFocustrap,
+  useMounted
+} from 'hooks';
 
 import ModalStyles from './Modal.module.scss';
 import { ModalProps } from './Modal.type';
@@ -17,8 +23,10 @@ export default function Modal({
   children,
   onHide,
   show,
+  className,
   ...props
 }: ModalProps) {
+  const isMounted = useMounted();
   const { current: showPrev } = usePrevious(show);
   const ref = useRef<HTMLDivElement>(null);
   const Portal = usePortal({
@@ -33,12 +41,12 @@ export default function Modal({
   });
 
   useEffect(() => {
-    if (showPrev && !show) {
+    if (showPrev && !show && isMounted.current) {
       setTimeout(Portal.unmount, 300);
     } else {
       Portal.mount(show);
     }
-  }, [Portal, show, showPrev]);
+  }, [Portal, isMounted, show, showPrev]);
   useClickaway(ref, () => onHide?.(), [showPrev, show]);
   useFocustrap(
     ref,
@@ -59,10 +67,10 @@ export default function Modal({
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             role="presentation"
-            className="pm-c-modal__overlay"
+            // todo: allow extend
+            className={cn(ModalStyles.root)}
             onKeyDown={handleKeyDown}
           >
-            {/** todo: fix type */}
             <motion.div
               ref={ref}
               initial={{ y: 16 }}
@@ -70,8 +78,7 @@ export default function Modal({
               exit={{ y: 16 }}
               role="dialog"
               aria-modal="true"
-              // todo: allow extend
-              className={cn(ModalStyles.root)}
+              className={cn(ModalStyles.dialog, className)}
               {...props}
             >
               {onHide && (
--- src/components/Modal/Modal.type.ts
+++ src/components/Modal/Modal.type.ts
@@ -1,6 +1,6 @@
-import React from 'react';
+import type { HTMLMotionProps } from 'framer-motion';
 
-export interface ModalProps extends React.ComponentPropsWithRef<'div'> {
+export interface ModalProps extends HTMLMotionProps<'div'> {
   onHide?(): void;
   show: boolean;
 }
