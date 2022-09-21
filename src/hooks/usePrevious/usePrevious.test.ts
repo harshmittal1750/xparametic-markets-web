31a39df1675b4855cd1bd9ec5791a60f6a784243
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-09-21 12:43:21 -0300

    test: add for `usePrevious`

--- src/hooks/usePrevious/usePrevious.test.ts
+++ src/hooks/usePrevious/usePrevious.test.ts
@@ -7,16 +7,25 @@ enum Value {
   Cur
 }
 
+function renderPrevious() {
+  return renderHook(initialProps => usePrevious(initialProps.value), {
+    initialProps: { value: Value.Prev }
+  });
+}
+
 describe('usePrevious', () => {
   it('returns the previous provided value', () => {
-    const { result, rerender } = renderHook(
-      initialProps => usePrevious(initialProps.value),
-      { initialProps: { value: Value.Prev } }
-    );
+    const { result, rerender } = renderPrevious();
 
     rerender({
       value: Value.Cur
     });
     expect(result.current.current).toBe(Value.Cur);
+  });
+  it('keeps the previous provided value on rerender', () => {
+    const { result, rerender } = renderPrevious();
+
+    rerender();
+    expect(result.current.current).toBe(Value.Prev);
   });
 });
