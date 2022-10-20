100d58bafb968162ade6d99b4587e2bf3dcb2c2b
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-10-20 20:54:47 -0300

    feat: add initial mobile navbar logic

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,16 +1,9 @@
-import { Link } from 'react-router-dom';
-
-import { PolkamarketsLogo } from 'assets/icons';
-
 import NavBarActions from './NavBarActions';
 import NavBarLinks from './NavBarLinks';
 
 export default function NavBar() {
   return (
     <div className="pm-l-navbar">
-      <Link to="/" aria-label="Homepage" className="pm-l-navbar__logos">
-        <PolkamarketsLogo />
-      </Link>
       <NavBarLinks />
       <NavBarActions />
     </div>
--- src/components/NavBar/NavBarLinks.tsx
+++ src/components/NavBar/NavBarLinks.tsx
@@ -1,28 +1,65 @@
-import * as RouterDom from 'react-router-dom';
+import { Fragment, useCallback } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+import { HamburguerMenuIcon, PolkamarketsLogo } from 'assets/icons';
+
+import { Button } from 'components/Button';
+
+import { useMedia, usePortal } from 'hooks';
 
 const marketsPathname = 'Markets';
 const pathnames = [marketsPathname, 'Portfolio', 'Achievements', 'Leaderboard'];
 
 export default function NavBarLinks() {
-  return (
-    <nav className="pm-l-navbar__nav">
-      {pathnames.map(_pathname => {
-        const pathname = `/${
-          _pathname === marketsPathname ? '' : _pathname.toLowerCase()
-        }`;
+  const isMx1024 = useMedia('(max-width: 1024px)');
+  const Portal = usePortal({
+    root: document.body,
+    onEffect() {
+      document.body.style.overflow = 'hidden';
 
-        return (
-          <RouterDom.NavLink
-            key={_pathname}
-            to={pathname}
-            className="pm-l-navbar__nav-link"
-            activeClassName="active"
-            isActive={(_, location) => location.pathname === pathname}
-          >
-            {_pathname}
-          </RouterDom.NavLink>
-        );
-      })}
+      return () => {
+        document.body.removeAttribute('style');
+      };
+    }
+  });
+  const handleClickMenu = useCallback(() => {
+    if (Portal.didMount) Portal.unmount();
+    else Portal.mount(true);
+  }, [Portal]);
+  const Ul = isMx1024 ? Portal : Fragment;
+
+  return (
+    <nav>
+      {isMx1024 && (
+        <Button color="default" size="sm" onClick={handleClickMenu}>
+          <HamburguerMenuIcon />
+        </Button>
+      )}
+      <Link to="/" aria-label="Homepage" className="pm-l-navbar__logos">
+        <PolkamarketsLogo />
+      </Link>
+      <Ul>
+        <ul className="pm-l-navbar__nav">
+          {pathnames.map(_pathname => {
+            const pathname = `/${
+              _pathname === marketsPathname ? '' : _pathname.toLowerCase()
+            }`;
+
+            return (
+              <li key={_pathname}>
+                <NavLink
+                  to={pathname}
+                  className="pm-l-navbar__nav-link"
+                  activeClassName="active"
+                  isActive={(_, location) => location.pathname === pathname}
+                >
+                  {_pathname}
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
+      </Ul>
     </nav>
   );
 }
