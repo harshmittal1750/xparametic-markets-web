4f6f3dfbaee258859d6fb50794084bb3089828ac
Wellington Matheus <w.matheus@outlook.com>
2022-10-20 10:34:11 -0300

    feat(VoteArrowsReducer): create loading mngmnt and actions

--- src/components/VoteArrows/VoteArrows.reducer.ts
+++ src/components/VoteArrows/VoteArrows.reducer.ts
@@ -1,10 +1,20 @@
+import { PolkamarketsApiService, PolkamarketsService } from 'services';
+
 import { VoteArrowsSentiment } from './VoteArrows.type';
 
 export enum VoteArrowsActions {
-  UPVOTE = 'UPVOTE',
-  DOWNVOTE = 'DOWNVOTE',
-  REMOVE_UPVOTE = 'REMOVE_UPVOTE',
-  REMOVE_DOWNVOTE = 'REMOVE_DOWNVOTE'
+  UPVOTE_REQUEST = 'UPVOTE_REQUEST',
+  UPVOTE_SUCCESS = 'UPVOTE_SUCCESS',
+  UPVOTE_FAILURE = 'UPVOTE_FAILURE',
+  DOWNVOTE_REQUEST = 'DOWNVOTE_REQUEST',
+  DOWNVOTE_SUCCESS = 'DOWNVOTE_SUCCESS',
+  DOWNVOTE_FAILURE = 'DOWNVOTE_FAILURE',
+  REMOVE_UPVOTE_REQUEST = 'REMOVE_UPVOTE_REQUEST',
+  REMOVE_UPVOTE_SUCCESS = 'REMOVE_UPVOTE_SUCCESS',
+  REMOVE_UPVOTE_FAILURE = 'REMOVE_UPVOTE_FAILURE',
+  REMOVE_DOWNVOTE_REQUEST = 'REMOVE_DOWNVOTE_REQUEST',
+  REMOVE_DOWNVOTE_SUCCESS = 'REMOVE_DOWNVOTE_SUCCESS',
+  REMOVE_DOWNVOTE_FAILURE = 'REMOVE_DOWNVOTE_FAILURE'
 }
 
 type VoteArrowsAction = {
@@ -14,7 +24,9 @@ type VoteArrowsAction = {
 export type VoteArrowsState = {
   initialCounter: number;
   counter: number;
+  initialSentiment: VoteArrowsSentiment;
   sentiment: VoteArrowsSentiment;
+  isLoading: boolean;
 };
 
 function voteArrowsReducer(
@@ -24,31 +36,81 @@ function voteArrowsReducer(
   const { type } = action;
 
   switch (type) {
-    case VoteArrowsActions.UPVOTE:
+    case VoteArrowsActions.UPVOTE_REQUEST:
       return {
         ...state,
         counter: state.initialCounter + 1,
-        sentiment:
-          state.counter + 1 === state.initialCounter ? 'neutral' : 'positive'
+        sentiment: 'positive',
+        isLoading: true
       };
-    case VoteArrowsActions.DOWNVOTE:
+    case VoteArrowsActions.UPVOTE_SUCCESS:
+      return {
+        ...state,
+        isLoading: false
+      };
+    case VoteArrowsActions.UPVOTE_FAILURE:
+      return {
+        ...state,
+        counter: state.initialCounter,
+        sentiment: state.initialSentiment,
+        isLoading: false
+      };
+    case VoteArrowsActions.DOWNVOTE_REQUEST:
       return {
         ...state,
         counter: state.initialCounter - 1,
-        sentiment:
-          state.counter - 1 === state.initialCounter ? 'neutral' : 'negative'
+        sentiment: 'negative',
+        isLoading: true
       };
-    case VoteArrowsActions.REMOVE_UPVOTE:
+    case VoteArrowsActions.DOWNVOTE_SUCCESS:
+      return {
+        ...state,
+        isLoading: false
+      };
+    case VoteArrowsActions.DOWNVOTE_FAILURE:
       return {
         ...state,
         counter: state.initialCounter,
-        sentiment: 'neutral'
+        sentiment: state.initialSentiment,
+        isLoading: false
       };
-    case VoteArrowsActions.REMOVE_DOWNVOTE:
+    case VoteArrowsActions.REMOVE_UPVOTE_REQUEST:
       return {
         ...state,
         counter: state.initialCounter,
-        sentiment: 'neutral'
+        sentiment: 'neutral',
+        isLoading: true
+      };
+    case VoteArrowsActions.REMOVE_UPVOTE_SUCCESS:
+      return {
+        ...state,
+        isLoading: false
+      };
+    case VoteArrowsActions.REMOVE_UPVOTE_FAILURE:
+      return {
+        ...state,
+        counter: state.initialCounter,
+        sentiment: state.initialSentiment,
+        isLoading: false
+      };
+    case VoteArrowsActions.REMOVE_DOWNVOTE_REQUEST:
+      return {
+        ...state,
+        counter: state.initialCounter,
+        sentiment: 'neutral',
+        isLoading: true
+      };
+    case VoteArrowsActions.REMOVE_DOWNVOTE_SUCCESS:
+      return {
+        ...state,
+        isLoading: false
+      };
+    case VoteArrowsActions.REMOVE_DOWNVOTE_FAILURE:
+      return {
+        ...state,
+        counter: state.initialCounter,
+        sentiment: state.initialSentiment,
+        isLoading: false
       };
     default:
       return state;
@@ -56,3 +118,85 @@ function voteArrowsReducer(
 }
 
 export default voteArrowsReducer;
+
+type VoteArrowsActionArgs = {
+  dispatch: (value: VoteArrowsAction) => void;
+  polkamarketsService: PolkamarketsService;
+  marketId: string;
+  polkamarketsApiService: PolkamarketsApiService;
+  marketSlug: string;
+};
+
+async function upvote({
+  dispatch,
+  polkamarketsService,
+  marketId,
+  polkamarketsApiService,
+  marketSlug
+}: VoteArrowsActionArgs) {
+  dispatch({ type: VoteArrowsActions.UPVOTE_REQUEST });
+
+  try {
+    await polkamarketsService.upvoteItem(marketId);
+    dispatch({ type: VoteArrowsActions.UPVOTE_SUCCESS });
+  } catch (error) {
+    dispatch({ type: VoteArrowsActions.UPVOTE_FAILURE });
+  }
+  polkamarketsApiService.reloadMarket(marketSlug);
+}
+
+async function removeUpvote({
+  dispatch,
+  polkamarketsService,
+  marketId,
+  polkamarketsApiService,
+  marketSlug
+}: VoteArrowsActionArgs) {
+  dispatch({ type: VoteArrowsActions.REMOVE_UPVOTE_REQUEST });
+
+  try {
+    await polkamarketsService.removeUpvoteItem(marketId);
+    dispatch({ type: VoteArrowsActions.REMOVE_UPVOTE_SUCCESS });
+  } catch (error) {
+    dispatch({ type: VoteArrowsActions.REMOVE_UPVOTE_FAILURE });
+  }
+  polkamarketsApiService.reloadMarket(marketSlug);
+}
+
+async function downvote({
+  dispatch,
+  polkamarketsService,
+  marketId,
+  polkamarketsApiService,
+  marketSlug
+}: VoteArrowsActionArgs) {
+  dispatch({ type: VoteArrowsActions.DOWNVOTE_REQUEST });
+
+  try {
+    await polkamarketsService.downvoteItem(marketId);
+    dispatch({ type: VoteArrowsActions.DOWNVOTE_SUCCESS });
+  } catch (error) {
+    dispatch({ type: VoteArrowsActions.DOWNVOTE_FAILURE });
+  }
+  polkamarketsApiService.reloadMarket(marketSlug);
+}
+
+async function removeDownvote({
+  dispatch,
+  polkamarketsService,
+  marketId,
+  polkamarketsApiService,
+  marketSlug
+}: VoteArrowsActionArgs) {
+  dispatch({ type: VoteArrowsActions.REMOVE_DOWNVOTE_REQUEST });
+
+  try {
+    await polkamarketsService.removeDownvoteItem(marketId);
+    dispatch({ type: VoteArrowsActions.REMOVE_DOWNVOTE_SUCCESS });
+  } catch (error) {
+    dispatch({ type: VoteArrowsActions.REMOVE_DOWNVOTE_FAILURE });
+  }
+  polkamarketsApiService.reloadMarket(marketSlug);
+}
+
+export { upvote, removeUpvote, downvote, removeDownvote };
