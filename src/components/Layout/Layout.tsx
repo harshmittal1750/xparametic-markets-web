0c5e5b0c5bbf056b94284fadf006ad09376535d5
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-10-20 14:34:46 -0300

    feat: simplify Layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import type React from 'react';
 
 import cn from 'classnames';
 
@@ -11,47 +11,39 @@ import RightSidebar from '../RightSidebar';
 import ScrollableArea from '../ScrollableArea';
 import Sidebar from '../Sidebar';
 
-type LayoutProps = {
-  children: ReactNode;
-};
-
-function Layout({ children }: LayoutProps) {
+export default function Layout({ children }: React.PropsWithChildren<{}>) {
   const { alertList } = useAlertNotification();
   const hasAlertNotification = alertList.size > 0;
 
   return (
-    <>
+    <div className="pm-l-layout">
       <BetaWarning />
-      <div className="pm-l-layout">
-        <header className="pm-l-layout__header sticky">
-          <div id="alert-notification-portal" className="pm-l-layout__alert" />
-          <NavBar />
-        </header>
-        <nav className="pm-l-layout__nav">
-          <Sidebar />
-        </nav>
-        <ScrollableArea
-          className={cn({
-            'pm-l-layout__scrollable-area--with-alert': hasAlertNotification,
-            'pm-l-layout__scrollable-area': !hasAlertNotification
-          })}
-        >
-          <main className="pm-l-layout__main">
-            {children}
-            <footer className="pm-l-layout__footer">
-              <Footer />
-            </footer>
-          </main>
-        </ScrollableArea>
-        <ScrollableArea>
-          <aside className="pm-l-layout__aside">
-            <RightSidebar hasAlertNotification={hasAlertNotification} />
-          </aside>
-        </ScrollableArea>
-        <div id="toast-notification-portal" />
-      </div>
-    </>
+      <header className="pm-l-layout__header sticky">
+        <div id="alert-notification-portal" className="pm-l-layout__alert" />
+        <NavBar />
+      </header>
+      <nav className="pm-l-layout__nav">
+        <Sidebar />
+      </nav>
+      <ScrollableArea
+        className={cn({
+          'pm-l-layout__scrollable-area--with-alert': hasAlertNotification,
+          'pm-l-layout__scrollable-area': !hasAlertNotification
+        })}
+      >
+        <main className="pm-l-layout__main">
+          {children}
+          <footer className="pm-l-layout__footer">
+            <Footer />
+          </footer>
+        </main>
+      </ScrollableArea>
+      <ScrollableArea>
+        <aside className="pm-l-layout__aside">
+          <RightSidebar hasAlertNotification={hasAlertNotification} />
+        </aside>
+      </ScrollableArea>
+      <div id="toast-notification-portal" />
+    </div>
   );
 }
-
-export default Layout;
