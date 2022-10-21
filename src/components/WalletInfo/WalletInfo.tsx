07fd8f5b78f8931f55005ea8daf817ae0a0b5a0d
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-10-21 14:11:19 -0300

    feat: adjust wallet info styles

--- src/components/WalletInfo/WalletInfo.tsx
+++ src/components/WalletInfo/WalletInfo.tsx
@@ -12,16 +12,12 @@ function WalletInfo() {
 
   return (
     <div className="pm-c-wallet-info">
-      <div
-        className="pm-c-button-outline--base pm-c-button--sm"
-        style={{ padding: '0.4rem 1.4rem', height: '100%' }}
-      >
+      <div className="pm-c-button-outline--base pm-c-button--sm pm-c-wallet-info__currency">
         {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
         {formatNumberToString(polkBalance)} POLK
         {network.buyEc20Url && (
           <a
-            style={{ padding: '0.5rem 1rem' }}
-            className="pm-c-button-normal--primary pm-c-button--sm"
+            className="pm-c-button-normal--primary pm-c-button--sm pm-c-wallet-info__currency__button"
             target="_blank"
             rel="noreferrer"
             href={network.buyEc20Url}
@@ -30,24 +26,11 @@ function WalletInfo() {
           </a>
         )}
       </div>
-      <div
-        className="pm-c-button-outline--base pm-c-button--sm"
-        style={{
-          padding: '0.4rem 1.4rem',
-          height: '100%'
-        }}
-      >
+      <div className="pm-c-button-outline--base pm-c-button--sm pm-c-wallet-info__currency">
         {/* eslint-disable react/jsx-one-expression-per-line */}
         {ethBalance.toFixed(4)}{' '}
         {network.currency.name.match(/\w{3}/)?.[0].toUpperCase()}
-        <span
-          className="pm-c-button-normal--default pm-c-button--sm"
-          style={{
-            padding: '0.5rem 1rem',
-            backgroundColor: 'rgb(255 255 255 / 0.1)',
-            borderColor: 'transparent'
-          }}
-        >
+        <span className="pm-c-button-normal--default pm-c-button--sm pm-c-wallet-info__currency__button pm-c-wallet-info__currency__address">
           {walletAddress.match(/^\d\w{4}|\d\w{4}$/gm)?.join('...')}
         </span>
         <Transak />
