418be2b8f97ef6f13a064b0a463c269c0de7601a
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-10-22 20:51:17 -0300

    feat: adjust theme switcher

--- src/components/NavBar/NavBarActions.tsx
+++ src/components/NavBar/NavBarActions.tsx
@@ -1,21 +1,33 @@
-import { EthereumIcon, SunIcon } from 'assets/icons';
+import { EthereumIcon, MoonIcon, SunIcon } from 'assets/icons';
 
 import { Button } from 'components/Button';
 import ConnectMetamask from 'components/ConnectMetamask';
 import WalletInfo from 'components/WalletInfo';
 
-import { useAppSelector } from 'hooks';
+import { useAppSelector, useTheme } from 'hooks';
 
 function NavBarActions() {
+  const theme = useTheme();
   const walletConnected = useAppSelector(
     state => state.polkamarkets.isLoggedIn
   );
+  const themeIcon = theme.theme === 'dark' ? <SunIcon /> : <MoonIcon />;
+  const themeAnti = theme.theme === 'dark' ? 'light' : 'dark';
+
+  function handleClickTheme() {
+    theme.setTheme(themeAnti);
+  }
 
   return (
     <div className="pm-l-layout__header__actions">
       {walletConnected ? <WalletInfo /> : <ConnectMetamask />}
-      <Button variant="outline" color="default">
-        <SunIcon />
+      <Button
+        variant="outline"
+        color="default"
+        aria-label={`Switch to ${themeAnti} theme`}
+        onClick={handleClickTheme}
+      >
+        {themeIcon}
       </Button>
       <Button variant="outline" color="default">
         <EthereumIcon />
