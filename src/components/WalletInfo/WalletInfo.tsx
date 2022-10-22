33ed889f93017b0dd768ebc374255d5c719681ab
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-10-22 20:26:40 -0300

    feat: adjust wallet info UI

--- src/components/WalletInfo/WalletInfo.tsx
+++ src/components/WalletInfo/WalletInfo.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import { formatNumberToString } from 'helpers/math';
 
 import { useAppSelector, useNetwork } from 'hooks';
@@ -12,10 +14,9 @@ function WalletInfo() {
 
   return (
     <div className="pm-c-wallet-info">
-      <div className="pm-c-button-outline--base pm-c-button--sm pm-c-wallet-info__currency">
-        {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
-        {formatNumberToString(polkBalance)}{' '}
-        <span className="pm-c-wallet-info__currency__ticker">POLK</span>
+      <div className="pm-c-wallet-info__currency">
+        {formatNumberToString(polkBalance)}
+        <span className="pm-c-wallet-info__currency__ticker"> POLK</span>
         {network.buyEc20Url && (
           <a
             className="pm-c-button-normal--primary pm-c-button--sm pm-c-wallet-info__currency__button"
@@ -23,19 +24,22 @@ function WalletInfo() {
             rel="noreferrer"
             href={network.buyEc20Url}
           >
-            Buy
+            Buy $POLK
           </a>
         )}
       </div>
-      <div className="pm-c-button-outline--base pm-c-button--sm pm-c-wallet-info__currency">
-        {/* eslint-disable react/jsx-one-expression-per-line */}
-        {ethBalance.toFixed(4)}{' '}
+      <div className="pm-c-wallet-info__currency">
+        {ethBalance.toFixed(4)}
         <span className="pm-c-wallet-info__currency__ticker">
+          {' '}
           {network.currency.name.match(/\w{3}/)?.[0].toUpperCase()}
         </span>
-        <span className="pm-c-button-normal--default pm-c-button--sm pm-c-wallet-info__currency__button pm-c-wallet-info__currency__address">
+        <Link
+          to={`/user/${walletAddress}`}
+          className="pm-c-button-subtle--default pm-c-button--sm pm-c-wallet-info__currency__button"
+        >
           {walletAddress.match(/^\d\w{4}|\d\w{4}$/gm)?.join('...')}
-        </span>
+        </Link>
         <Transak />
       </div>
     </div>
