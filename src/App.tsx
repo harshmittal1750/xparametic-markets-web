54cb7981c96ac984cfb639fe7a94051128e7faea
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-11-07 16:57:11 -0300

    Revert "feat: adjust how typefaces and tokens are served"
    This reverts commit 9a1043fcb861d3c059d4ea567e924d287933aef7.

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,6 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import store from 'redux/store';
 
-import './ui/useTheme/useTheme.scss';
-
 export default function App() {
   return (
     <Provider store={store}>
