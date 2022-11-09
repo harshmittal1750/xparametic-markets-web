d22882396eabc12f5bc149b2ce0da081ac6a1ae1
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-11-09 15:09:50 -0300

    feat: create `Divider`

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -2,6 +2,8 @@ export { default as Adornment } from './Adornment';
 
 export { default as ButtonGroup } from './ButtonGroup';
 
+export { default as Divider } from './Divider';
+
 export { default as Hero } from './Hero';
 
 export { default as List } from './List';
