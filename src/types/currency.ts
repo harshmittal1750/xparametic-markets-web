4e192804499789a840859f6a4d0dae0442199138
Wellington Matheus <w.matheus@outlook.com>
2022-11-09 11:01:25 -0300

    feat(CurrencyType): add `iconName` attribute

--- src/types/currency.ts
+++ src/types/currency.ts
@@ -1,8 +1,11 @@
 import { ReactNode } from 'react';
 
+import type { IconProps } from 'components/Icon';
+
 export type Currency = {
   name: string;
   ticker: string;
   symbol: string;
   icon: ReactNode;
+  iconName: IconProps['name'];
 };
