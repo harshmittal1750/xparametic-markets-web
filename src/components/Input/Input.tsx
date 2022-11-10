354ac009b363b8507cbf6216016cd96997ec1854
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-11-10 19:53:25 -0300

    feat: add liquidity list filter

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
+import cn from 'classnames';
 import { useField } from 'formik';
 
 import InputErrorMessage from './InputErrorMessage';
 
 type InputProps = {
-  label: string;
+  label?: string;
   name: string;
   description?: string;
 };
@@ -13,22 +14,30 @@ type InputProps = {
 const Input = React.forwardRef<
   HTMLInputElement,
   InputProps & React.InputHTMLAttributes<HTMLInputElement>
->(({ label, name, description, ...props }, ref) => {
+>(({ label, name, description, className, ...props }, ref) => {
   const [field, meta] = useField(name);
 
   const hasError = meta.touched && meta.error;
 
   return (
     <div className="pm-c-input__group">
-      <label
-        htmlFor={name}
-        className={`pm-c-input__label--${hasError ? 'error' : 'default'}`}
-      >
-        {label}
-      </label>
+      {!!label && (
+        <label
+          htmlFor={name}
+          className={`pm-c-input__label--${hasError ? 'error' : 'default'}`}
+        >
+          {label}
+        </label>
+      )}
       <input
         ref={ref}
-        className={`pm-c-input--${hasError ? 'error' : 'default'}`}
+        className={cn(
+          {
+            'pm-c-input--error': hasError,
+            'pm-c-input--default': !hasError
+          },
+          className
+        )}
         id={name}
         {...field}
         {...props}
--- src/pages/Home/HomeNavFilter.tsx
+++ src/pages/Home/HomeNavFilter.tsx
@@ -5,13 +5,21 @@ import { Form, Formik } from 'formik';
 import { AnimatePresence, motion } from 'framer-motion';
 import { Adornment, Divider, List, ListItem, ListItemText, Toggle } from 'ui';
 
-import { Button, DateInput, Icon, Modal, ToggleSwitch } from 'components';
+import {
+  Input,
+  Button,
+  DateInput,
+  Icon,
+  Modal,
+  ToggleSwitch
+} from 'components';
 
 export default function HomeNavFilter() {
   const [show, setShow] = useState(false);
   const [expand, setExpand] = useState({
     network: false,
-    endDate: false
+    endDate: false,
+    liquidity: false
   });
   const [toggle, setToggle] = useState({
     network: {
@@ -24,6 +32,13 @@ export default function HomeNavFilter() {
       'Ends today': false,
       'Ends this week': false,
       'Ends this month': false,
+      Custom: false
+    },
+    liquidity: {
+      Any: false,
+      'Under $5,000': false,
+      '$5,000 - $10,000': false,
+      'Over $10,000': false,
       Custom: false
     }
   });
@@ -72,125 +87,103 @@ export default function HomeNavFilter() {
         exit={{ x: -304 }}
         // onHide={handleHide}
       >
-        <List
-          style={{
-            width: 304,
-            height: '100%',
-            borderRight: '1px solid var(--color-border)',
-            backgroundColor: 'var(--color-surface)'
+        <Formik
+          onSubmit={() => {}}
+          initialValues={{
+            start: null,
+            end: null,
+            min: '',
+            max: ''
           }}
         >
-          <ListItem>
-            <ListItemText>Verified</ListItemText>
-            <Adornment edge="end">
-              <ToggleSwitch name="verified" onChange={() => {}} />
-            </Adornment>
-          </ListItem>
-          <ListItem>
-            <ListItemText>Favorites</ListItemText>
-            <Adornment edge="end">
-              <ToggleSwitch name="favorites" onChange={() => {}} />
-            </Adornment>
-          </ListItem>
-          <Divider />
-          <ListItem onClick={handleExpand('network')}>
-            <ListItemText>Network</ListItemText>
-            <Adornment edge="end">
-              <Icon
-                name="Chevron"
-                size="lg"
-                dir={expand.network ? 'up' : 'down'}
-              />
-            </Adornment>
-          </ListItem>
-          <AnimatePresence>
-            {expand.network && (
-              <motion.div
-                style={{ overflow: 'hidden' }}
-                initial={{ height: 0 }}
-                animate={{ height: 'auto' }}
-                exit={{ height: 0 }}
-              >
-                <List
-                  style={{
-                    backgroundColor: 'var(--color-background-primary)'
-                  }}
+          <List className="pm-p-home__filter-list">
+            <ListItem>
+              <ListItemText>Verified</ListItemText>
+              <Adornment edge="end">
+                <ToggleSwitch name="verified" onChange={() => {}} />
+              </Adornment>
+            </ListItem>
+            <ListItem>
+              <ListItemText>Favorites</ListItemText>
+              <Adornment edge="end">
+                <ToggleSwitch name="favorites" onChange={() => {}} />
+              </Adornment>
+            </ListItem>
+            <Divider />
+            <ListItem onClick={handleExpand('network')}>
+              <ListItemText>Network</ListItemText>
+              <Adornment edge="end">
+                <Icon
+                  name="Chevron"
+                  size="lg"
+                  dir={expand.network ? 'up' : 'down'}
+                />
+              </Adornment>
+            </ListItem>
+            <AnimatePresence>
+              {expand.network && (
+                <motion.div
+                  style={{ overflow: 'hidden' }}
+                  initial={{ height: 0 }}
+                  animate={{ height: 'auto' }}
+                  exit={{ height: 0 }}
                 >
-                  {Object.keys(toggle.network).map(network => (
-                    <ListItem key={network}>
-                      <ListItemText>{network}</ListItemText>
-                      <Adornment edge="end">
-                        <Toggle
-                          type="checkbox"
-                          checked={toggle.network[network]}
-                          value={network}
-                          name={network}
-                          onChange={handleToggle('network')}
-                        />
-                      </Adornment>
-                    </ListItem>
-                  ))}
-                </List>
-              </motion.div>
-            )}
-          </AnimatePresence>
-          <Divider />
-          <ListItem onClick={handleExpand('endDate')}>
-            <ListItemText>End Date</ListItemText>
-            <Adornment edge="end">
-              <Icon
-                name="Chevron"
-                size="lg"
-                dir={expand.endDate ? 'up' : 'down'}
-              />
-            </Adornment>
-          </ListItem>
-          <AnimatePresence>
-            {expand.endDate && (
-              <motion.div
-                style={{ overflow: 'hidden' }}
-                initial={{ height: 0 }}
-                animate={{ height: 'auto' }}
-                exit={{ height: 0 }}
-              >
-                <List
-                  style={{
-                    backgroundColor: 'var(--color-background-primary)'
-                  }}
-                >
-                  {Object.keys(toggle.endDate).map(endDate => (
-                    <Fragment key={endDate}>
-                      <ListItem>
-                        <ListItemText>{endDate}</ListItemText>
+                  <List className="pm-p-home__filter-list-sublist">
+                    {Object.keys(toggle.network).map(network => (
+                      <ListItem key={network}>
+                        <ListItemText>{network}</ListItemText>
                         <Adornment edge="end">
                           <Toggle
                             type="checkbox"
-                            checked={toggle.endDate[endDate]}
-                            value={endDate}
-                            name={endDate}
-                            onChange={handleToggle('endDate')}
+                            checked={toggle.network[network]}
+                            value={network}
+                            name={network}
+                            onChange={handleToggle('network')}
                           />
                         </Adornment>
                       </ListItem>
-                      {endDate === 'Custom' && (
+                    ))}
+                  </List>
+                </motion.div>
+              )}
+            </AnimatePresence>
+            <Divider />
+            <ListItem onClick={handleExpand('endDate')}>
+              <ListItemText>End Date</ListItemText>
+              <Adornment edge="end">
+                <Icon
+                  name="Chevron"
+                  size="lg"
+                  dir={expand.endDate ? 'up' : 'down'}
+                />
+              </Adornment>
+            </ListItem>
+            <AnimatePresence>
+              {expand.endDate && (
+                <motion.div
+                  style={{ overflow: 'hidden' }}
+                  initial={{ height: 0 }}
+                  animate={{ height: 'auto' }}
+                  exit={{ height: 0 }}
+                >
+                  <List className="pm-p-home__filter-list-sublist">
+                    {Object.keys(toggle.endDate).map(endDate => (
+                      <Fragment key={endDate}>
                         <ListItem>
-                          <Formik
-                            onSubmit={() => {}}
-                            initialValues={{
-                              start: null,
-                              end: null
-                            }}
-                          >
-                            <Form
-                              style={{
-                                display: 'flex',
-                                gap: 12,
-                                alignItems: 'center',
-                                marginBottom: 12,
-                                marginRight: -12,
-                                marginLeft: -12
-                              }}
-                            >
+                          <ListItemText>{endDate}</ListItemText>
+                          <Adornment edge="end">
+                            <Toggle
+                              type="checkbox"
+                              checked={toggle.endDate[endDate]}
+                              value={endDate}
+                              name={endDate}
+                              onChange={handleToggle('endDate')}
+                            />
+                          </Adornment>
+                        </ListItem>
+                        {endDate === 'Custom' && (
+                          <ListItem>
+                            <div className="pm-p-home__filter-list-sublist-inputs">
                               <DateInput
                                 name="start"
                                 emptyLabel="Start"
@@ -202,17 +195,74 @@ export default function HomeNavFilter() {
                                 emptyLabel="End"
                                 format="DD/MM/YYYY"
                               />
-                            </Form>
-                          </Formik>
+                            </div>
+                          </ListItem>
+                        )}
+                      </Fragment>
+                    ))}
+                  </List>
+                </motion.div>
+              )}
+            </AnimatePresence>
+            <Divider />
+            <ListItem onClick={handleExpand('liquidity')}>
+              <ListItemText>Liquidity</ListItemText>
+              <Adornment edge="end">
+                <Icon
+                  name="Chevron"
+                  size="lg"
+                  dir={expand.liquidity ? 'up' : 'down'}
+                />
+              </Adornment>
+            </ListItem>
+            <AnimatePresence>
+              {expand.liquidity && (
+                <motion.div
+                  style={{ overflow: 'hidden' }}
+                  initial={{ height: 0 }}
+                  animate={{ height: 'auto' }}
+                  exit={{ height: 0 }}
+                >
+                  <List className="pm-p-home__filter-list-sublist">
+                    {Object.keys(toggle.liquidity).map(liquidity => (
+                      <Fragment key={liquidity}>
+                        <ListItem>
+                          <ListItemText>{liquidity}</ListItemText>
+                          <Adornment edge="end">
+                            <Toggle
+                              type="checkbox"
+                              checked={toggle.liquidity[liquidity]}
+                              value={liquidity}
+                              name={liquidity}
+                              onChange={handleToggle('liquidity')}
+                            />
+                          </Adornment>
                         </ListItem>
-                      )}
-                    </Fragment>
-                  ))}
-                </List>
-              </motion.div>
-            )}
-          </AnimatePresence>
-        </List>
+                        {liquidity === 'Custom' && (
+                          <ListItem>
+                            <div className="pm-p-home__filter-list-sublist-inputs">
+                              <Input
+                                name="min"
+                                placeholder="Min"
+                                className="pm-p-home__filter-list-sublist-inputs-input"
+                              />
+                              to
+                              <Input
+                                name="max"
+                                placeholder="Max"
+                                className="pm-p-home__filter-list-sublist-inputs-input"
+                              />
+                            </div>
+                          </ListItem>
+                        )}
+                      </Fragment>
+                    ))}
+                  </List>
+                </motion.div>
+              )}
+            </AnimatePresence>
+          </List>
+        </Formik>
       </Modal>
     </>
   );
