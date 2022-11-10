845da4a277efcbb643584c18c5f48dfec58bc311
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-11-10 16:02:46 -0300

    feat: add end date list item

--- src/pages/Home/HomeNavFilter.tsx
+++ src/pages/Home/HomeNavFilter.tsx
@@ -1,65 +1,46 @@
-import { useState } from 'react';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Fragment, useState } from 'react';
 
+import { Form, Formik } from 'formik';
 import { AnimatePresence, motion } from 'framer-motion';
 import { Adornment, Divider, List, ListItem, ListItemText, Radio } from 'ui';
 
-import { Button, Icon, Modal, ToggleSwitch } from 'components';
+import { Button, DateInput, Icon, Modal, ToggleSwitch } from 'components';
 
-function ListItemNested() {
-  const [expand, setExpand] = useState(false);
-  const [radio, setRadio] = useState('Binance');
-
-  function handleExpand() {
-    setExpand(prevExpand => !prevExpand);
-  }
-  function handleRadio(event: React.ChangeEvent<HTMLInputElement>) {
-    setRadio(event.target.value);
-  }
-
-  return (
-    <>
-      <ListItem onClick={handleExpand}>
-        <ListItemText>Network</ListItemText>
-        <Adornment edge="end">
-          <Icon name="Chevron" size="lg" dir={expand ? 'up' : 'down'} />
-        </Adornment>
-      </ListItem>
-      <AnimatePresence>
-        {expand && (
-          <motion.div
-            style={{ overflow: 'hidden' }}
-            initial={{ height: 0 }}
-            animate={{ height: 'auto' }}
-            exit={{ height: 0 }}
-          >
-            <List
-              style={{
-                backgroundColor: 'var(--color-background-primary)'
-              }}
-            >
-              {['Binance', 'Moonbeam', 'Ethereum'].map(network => (
-                <ListItem key={network}>
-                  <ListItemText>{network}</ListItemText>
-                  <Adornment edge="end">
-                    <Radio
-                      checked={network === radio}
-                      value={network}
-                      onChange={handleRadio}
-                    />
-                  </Adornment>
-                </ListItem>
-              ))}
-            </List>
-          </motion.div>
-        )}
-      </AnimatePresence>
-    </>
-  );
-}
+const networks = ['Binance', 'Moonbeam', 'Ethereum'];
+const endDates = [
+  'Any',
+  'Ends today',
+  'Ends this week',
+  'Ends this month',
+  'Custom'
+];
 
 export default function HomeNavFilter() {
   const [show, setShow] = useState(false);
+  const [expand, setExpand] = useState({
+    network: false,
+    endDate: false
+  });
+  const [radio, setRadio] = useState({
+    network: networks[0],
+    endDate: endDates[0]
+  });
 
+  function handleExpand(name: string) {
+    return () =>
+      setExpand(prevExpand => ({
+        ...prevExpand,
+        [name]: !prevExpand[name]
+      }));
+  }
+  function handleRadio(name: string) {
+    return (event: React.ChangeEvent<HTMLInputElement>) =>
+      setRadio(prevRadio => ({
+        ...prevRadio,
+        [name]: event.target.value
+      }));
+  }
   function handleShow() {
     setShow(true);
   }
@@ -85,11 +66,11 @@ export default function HomeNavFilter() {
         initial={{ x: -256 }}
         animate={{ x: 0 }}
         exit={{ x: -256 }}
-        onHide={handleHide}
+        // onHide={handleHide}
       >
         <List
           style={{
-            width: 256,
+            width: 304,
             height: '100%',
             borderRight: '1px solid var(--color-border)',
             backgroundColor: 'var(--color-surface)'
@@ -108,9 +89,111 @@ export default function HomeNavFilter() {
             </Adornment>
           </ListItem>
           <Divider />
-          <ListItemNested />
+          <ListItem onClick={handleExpand('network')}>
+            <ListItemText>Network</ListItemText>
+            <Adornment edge="end">
+              <Icon
+                name="Chevron"
+                size="lg"
+                dir={expand.network ? 'up' : 'down'}
+              />
+            </Adornment>
+          </ListItem>
+          <AnimatePresence>
+            {expand.network && (
+              <motion.div
+                style={{ overflow: 'hidden' }}
+                initial={{ height: 0 }}
+                animate={{ height: 'auto' }}
+                exit={{ height: 0 }}
+              >
+                <List
+                  style={{
+                    backgroundColor: 'var(--color-background-primary)'
+                  }}
+                >
+                  {networks.map(network => (
+                    <ListItem key={network}>
+                      <ListItemText>{network}</ListItemText>
+                      <Adornment edge="end">
+                        <Radio
+                          checked={network === radio.network}
+                          value={network}
+                          onChange={handleRadio('network')}
+                        />
+                      </Adornment>
+                    </ListItem>
+                  ))}
+                </List>
+              </motion.div>
+            )}
+          </AnimatePresence>
           <Divider />
-          <ListItemNested />
+          <ListItem onClick={handleExpand('endDate')}>
+            <ListItemText>End Date</ListItemText>
+            <Adornment edge="end">
+              <Icon
+                name="Chevron"
+                size="lg"
+                dir={expand.endDate ? 'up' : 'down'}
+              />
+            </Adornment>
+          </ListItem>
+          <AnimatePresence>
+            {expand.endDate && (
+              <motion.div
+                style={{ overflow: 'hidden' }}
+                initial={{ height: 0 }}
+                animate={{ height: 'auto' }}
+                exit={{ height: 0 }}
+              >
+                <List
+                  style={{
+                    backgroundColor: 'var(--color-background-primary)'
+                  }}
+                >
+                  {endDates.map(endDate => (
+                    <Fragment key={endDate}>
+                      <ListItem>
+                        <ListItemText>{endDate}</ListItemText>
+                        <Adornment edge="end">
+                          <Radio
+                            checked={endDate === radio.endDate}
+                            value={endDate}
+                            onChange={handleRadio('endDate')}
+                          />
+                        </Adornment>
+                      </ListItem>
+                      {endDate === 'Custom' && (
+                        <ListItem>
+                          <Formik
+                            onSubmit={() => {}}
+                            initialValues={{
+                              start: '',
+                              end: ''
+                            }}
+                          >
+                            <Form
+                              style={{
+                                display: 'flex',
+                                gap: 12,
+                                alignItems: 'center',
+                                marginBottom: 12
+                              }}
+                            >
+                              <DateInput name="start" format="DD/MM/YYYY" />
+                              to
+                              <DateInput name="end" format="DD/MM/YYYY" />
+                            </Form>
+                          </Formik>
+                        </ListItem>
+                      )}
+                    </Fragment>
+                  ))}
+                </List>
+              </motion.div>
+            )}
+          </AnimatePresence>
         </List>
       </Modal>
     </>
