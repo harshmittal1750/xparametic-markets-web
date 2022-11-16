81edb855e1eeb87b094106169f962c0ae3423f70
Wellington Matheus <w.matheus@outlook.com>
2022-11-16 15:23:56 -0300

    feat(WalletInfo): create `WalletInfoClaim`

---
+++ src/components/WalletInfo/WalletInfoClaim.tsx
@@ -0,0 +1,84 @@
+import { useCallback, useState } from 'react';
+
+import { login } from 'redux/ducks/polkamarkets';
+import { PolkamarketsService } from 'services';
+import { Transaction } from 'types/transaction';
+
+import Toast from 'components/Toast';
+import ToastNotification from 'components/ToastNotification';
+
+import { useAppDispatch, useAppSelector, useNetwork } from 'hooks';
+import useToastNotification from 'hooks/useToastNotification';
+
+import { Button } from '../Button';
+
+function WalletInfoClaim() {
+  const dispatch = useAppDispatch();
+  const { networkConfig } = useNetwork();
+  const { show: showToastNotification, close: closeToastNotification } =
+    useToastNotification();
+
+  const [transaction, setTransaction] = useState<Transaction>({
+    state: 'not_started'
+  });
+
+  const isPolkClaimed = useAppSelector(state => state.polkamarkets.polkClaimed);
+
+  const [isClaiming, setIsClaiming] = useState(false);
+
+  const handleClaim = useCallback(async () => {
+    const polkamarketsService = new PolkamarketsService(networkConfig);
+
+    try {
+      setTransaction({ state: 'request' });
+      setIsClaiming(true);
+
+      // performing claim action on smart contract
+      await polkamarketsService.claimPolk();
+      setTransaction({ state: 'success' });
+      setIsClaiming(false);
+
+      showToastNotification('claim-success');
+
+      // updating wallet
+      await dispatch(login(networkConfig));
+    } catch (error) {
+      setIsClaiming(false);
+      setTransaction({ state: 'failure' });
+    }
+  }, [dispatch, networkConfig, showToastNotification]);
+
+  return (
+    <>
+      {transaction.state === 'success' ? (
+        <ToastNotification id="claim-success" duration={10000}>
+          <Toast
+            variant="success"
+            title="Success"
+            description="Your transaction is completed!"
+          >
+            <Toast.Actions>
+              <Button
+                size="sm"
+                variant="ghost"
+                onClick={() => closeToastNotification('claim-success')}
+              >
+                Dismiss
+              </Button>
+            </Toast.Actions>
+          </Toast>
+        </ToastNotification>
+      ) : null}
+      <Button
+        className="pm-c-button-normal--primary pm-c-button--sm pm-c-wallet-info__currency__button"
+        loading={isClaiming}
+        disabled={isPolkClaimed || isClaiming}
+        onClick={handleClaim}
+      >
+        {isPolkClaimed ? '$IFL Claimed' : 'Claim $IFL'}
+      </Button>
+    </>
+  );
+}
+
+export default WalletInfoClaim;
