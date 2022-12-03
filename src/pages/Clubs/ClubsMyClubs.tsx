69bed72897d6e29dec8def44f23da384d5512286
Wellington Matheus <w.matheus@outlook.com>
2022-12-03 10:33:36 -0300

    feat(Pages): create Clubs

---
+++ src/pages/Clubs/ClubsMyClubs.tsx
@@ -0,0 +1,69 @@
+import { memo } from 'react';
+import { Link } from 'react-router-dom';
+
+import cn from 'classnames';
+import isEmpty from 'lodash/isEmpty';
+import { useGetLeaderboardGroupsByUserQuery } from 'services/Polkamarkets';
+
+import { CrownIcon } from 'assets/icons';
+
+import { AlertMini } from 'components';
+
+import ClubsMyClubsClasses from './ClubsMyClubs.module.scss';
+
+type LeaderboardMyLeaderboardsProps = {
+  loggedInUser?: string;
+};
+
+function LeaderboardMyLeaderboards({
+  loggedInUser
+}: LeaderboardMyLeaderboardsProps) {
+  const { data, isLoading } = useGetLeaderboardGroupsByUserQuery(
+    {
+      user: loggedInUser || ''
+    },
+    { skip: !loggedInUser }
+  );
+
+  return (
+    <div className="pm-c-leaderboard-stats bg-3 border-radius-medium border-solid border-1 width-full">
+      <h2 className="body semibold text-1">My Clubs</h2>
+      {isLoading ? (
+        <div className="flex-row justify-center align-center width-full padding-y-5 padding-x-4">
+          <span className="spinner--primary" />
+        </div>
+      ) : null}
+      {!isLoading && isEmpty(data) ? (
+        <AlertMini
+          style={{ border: 'none' }}
+          styles="outline"
+          variant="information"
+          description="You don't belong to any Clubs yet. Create a Club, or use the invite link if you have received one"
+        />
+      ) : null}
+      {!isLoading && !isEmpty(data) ? (
+        <ul className="flex-column gap-4">
+          {data?.map(leaderboard => (
+            <li key={leaderboard.slug} className="flex-row gap-3 align-center">
+              {leaderboard.admin ? <CrownIcon /> : null}
+              <Link
+                className={cn(
+                  ClubsMyClubsClasses.clubTitle,
+                  'tiny-uppercase',
+                  'bold',
+                  'text-3',
+                  'text-1-on-hover'
+                )}
+                to={`/clubs/${leaderboard.slug}`}
+              >
+                {leaderboard.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      ) : null}
+    </div>
+  );
+}
+
+export default memo(LeaderboardMyLeaderboards);
