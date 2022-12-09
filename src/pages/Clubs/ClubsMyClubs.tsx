7be490b6972539c99b62a12838bf5edb71111582
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-12-09 10:54:24 -0300

    feat: add placeholder for club avatar

--- src/pages/Clubs/ClubsMyClubs.tsx
+++ src/pages/Clubs/ClubsMyClubs.tsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import cn from 'classnames';
 import isEmpty from 'lodash/isEmpty';
 import { useGetLeaderboardGroupsByUserQuery } from 'services/Polkamarkets';
+import type { GetLeaderboardGroupsByUserData } from 'services/Polkamarkets/types';
 
 import { AlertMini, Icon } from 'components';
 
@@ -13,21 +14,31 @@ type LeaderboardMyLeaderboardsProps = {
   loggedInUser?: string;
 };
 
-function LeaderboardMyLeaderboards({
-  loggedInUser
-}: LeaderboardMyLeaderboardsProps) {
-  const { data, isLoading } = useGetLeaderboardGroupsByUserQuery(
-    { user: loggedInUser || '' },
-    { skip: !loggedInUser }
-  );
-  const renderList = isEmpty(data) ? (
-    <AlertMini
-      style={{ border: 'none' }}
-      styles="outline"
-      variant="information"
-      description="You don't belong to any Clubs yet. Create a Club, or use the invite link if you have received one"
-    />
-  ) : (
+function MyClubs({
+  data,
+  isLoading
+}: {
+  data?: GetLeaderboardGroupsByUserData;
+  isLoading: boolean;
+}) {
+  if (isLoading)
+    return (
+      <div className="flex-row justify-center align-center width-full padding-y-5 padding-x-4">
+        <span className="spinner--primary" />
+      </div>
+    );
+
+  if (isEmpty(data))
+    return (
+      <AlertMini
+        style={{ border: 'none' }}
+        styles="outline"
+        variant="information"
+        description="You don't belong to any Clubs yet. Create a Club, or use the invite link if you have received one"
+      />
+    );
+
+  return (
     <ul className="flex-column gap-4">
       {data?.map(leaderboard => (
         <li key={leaderboard.slug}>
@@ -35,9 +46,25 @@ function LeaderboardMyLeaderboards({
             to={`/clubs/${leaderboard.slug}`}
             className="flex-row gap-3 align-center"
           >
-            <div className={ClubsMyClubsClasses.avatar} />
+            <div className={ClubsMyClubsClasses.avatar}>
+              {leaderboard.imageUrl ? (
+                <img
+                  src={leaderboard.imageUrl}
+                  alt="Club Avatar"
+                  className={ClubsMyClubsClasses.image}
+                />
+              ) : (
+                <p
+                  className={cn(
+                    'body text-3 bold',
+                    ClubsMyClubsClasses.initials
+                  )}
+                >
+                  {leaderboard.title.match(/\w/)}
+                </p>
+              )}
+            </div>
             <div>
-              <p className="body text-1">{leaderboard.title}</p>
               <span
                 className={cn(
                   ClubsMyClubsClasses.clubTitle,
@@ -46,30 +73,35 @@ function LeaderboardMyLeaderboards({
               >
                 {leaderboard.admin ? (
                   <>
-                    <Icon name="Crown" className={ClubsMyClubsClasses.icon} />
+                    <span className={ClubsMyClubsClasses.adornment}>
+                      <Icon name="Crown" />
+                    </span>
                     Admin
                   </>
                 ) : (
                   'Member'
                 )}
               </span>
+              <p className="body text-1">{leaderboard.title}</p>
             </div>
           </Link>
         </li>
       ))}
     </ul>
   );
+}
+function LeaderboardMyLeaderboards({
+  loggedInUser
+}: LeaderboardMyLeaderboardsProps) {
+  const leaderboardGroupsByUserQuery = useGetLeaderboardGroupsByUserQuery(
+    { user: loggedInUser || '' },
+    { skip: !loggedInUser }
+  );
 
   return (
     <div className="pm-c-leaderboard-stats bg-3 border-radius-medium border-solid border-1 width-full">
       <h2 className="body semibold text-1">My Clubs</h2>
-      {isLoading ? (
-        <div className="flex-row justify-center align-center width-full padding-y-5 padding-x-4">
-          <span className="spinner--primary" />
-        </div>
-      ) : (
-        renderList
-      )}
+      <MyClubs {...leaderboardGroupsByUserQuery} />
     </div>
   );
 }
