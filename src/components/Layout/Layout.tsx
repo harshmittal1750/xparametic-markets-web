6431c139971cec82241c085888b2458331de52a6
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-12-16 08:18:47 -0300

    feat: remove background gradient

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,10 +1,5 @@
 import { Container } from 'ui';
 
-import bodyGradientDark from 'assets/images/body-gradient--dark.jpg';
-import bodyGradient from 'assets/images/body-gradient.jpg';
-
-import { useTheme } from 'hooks';
-
 import BetaWarning from '../BetaWarning';
 import Footer from '../Footer';
 import Header from '../Header';
@@ -12,20 +7,8 @@ import RightSidebar from '../RightSidebar';
 import ScrollableArea from '../ScrollableArea';
 
 export default function Layout({ children }: React.PropsWithChildren<{}>) {
-  const theme = useTheme();
-
   return (
     <div className="pm-l-layout">
-      <div
-        className="pm-l-layout__body-gradient"
-        style={
-          {
-            '--image': `url(${
-              theme.theme === 'dark' ? bodyGradientDark : bodyGradient
-            })`
-          } as React.CSSProperties
-        }
-      />
       <BetaWarning />
       <Header />
       <ScrollableArea className="pm-l-layout__scrollable-area">
