6b1aa39ed2d8ec9b9ed12dd47799e04e74a5bf3f
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-12-20 21:04:18 -0300

    feat: create hook to handle bouding client rect

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -18,3 +18,5 @@ export { default as ListItemText } from './ListItemText';
 export { default as Toggle } from './Toggle';
 
 export { default as useMedia } from './useMedia';
+
+export { default as useRect } from './useRect';
