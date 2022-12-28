ffe1c87890f1b7beba76cdaa772649b0c17de040
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-12-28 15:45:26 -0300

    feat: adjust market list UI container

--- src/components/MarketList/MarketListAsync.tsx
+++ src/components/MarketList/MarketListAsync.tsx
@@ -6,7 +6,6 @@ import {
   marketsSelector
 } from 'redux/ducks/markets';
 import { useAppDispatch } from 'redux/store';
-import { Container } from 'ui';
 
 import { InfoIcon } from 'assets/icons';
 
@@ -47,68 +46,58 @@ export default function MarketListAsync() {
     dispatch(getFavoriteMarkets(favoriteMarkets.favoriteMarkets));
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [dispatch]);
-  // prettier-ignore
-  // eslint-disable-next-line no-nested-ternary
-  const state = isLoading ? 'loading' : error ? 'error' : !markets.length ? 'warning' : 'success';
 
   useEffect(() => {
     handleMarkets();
   }, [handleMarkets]);
 
-  return (
-    <Container
-      style={{
-        paddingTop: 'var(--grid-margin)',
-        paddingBottom: 'var(--grid-margin)'
-      }}
-    >
-      {
-        {
-          loading: (
-            <div className="pm-c-market-list__empty-state">
-              <div className="pm-c-market-list__empty-state__body">
-                <span className="spinner--primary" />
-              </div>
-            </div>
-          ),
-          error: (
-            <div className="pm-c-market-list__error">
-              <div className="pm-c-market-list__error__body">
-                <InfoIcon />
-                <Text
-                  as="p"
-                  scale="tiny"
-                  fontWeight="semibold"
-                  className="pm-c-market-list__empty-state__body-description"
-                >
-                  Error fetching markets
-                </Text>
-              </div>
-              <div className="pm-c-market-list__error__actions">
-                <Button color="primary" size="sm" onClick={handleMarkets}>
-                  Try again
-                </Button>
-              </div>
-            </div>
-          ),
-          warning: (
-            <div className="pm-c-market-list__empty-state">
-              <div className="pm-c-market-list__empty-state__body">
-                <InfoIcon />
-                <Text
-                  as="p"
-                  scale="tiny"
-                  fontWeight="semibold"
-                  className="pm-c-market-list__empty-state__body-description"
-                >
-                  There are no available markets for this category.
-                </Text>
-              </div>
-            </div>
-          ),
-          success: <MarketList markets={markets} />
-        }[state]
-      }
-    </Container>
-  );
+  if (isLoading)
+    return (
+      <div className="pm-c-market-list__empty-state">
+        <div className="pm-c-market-list__empty-state__body">
+          <span className="spinner--primary" />
+        </div>
+      </div>
+    );
+
+  if (error)
+    return (
+      <div className="pm-c-market-list__error">
+        <div className="pm-c-market-list__error__body">
+          <InfoIcon />
+          <Text
+            as="p"
+            scale="tiny"
+            fontWeight="semibold"
+            className="pm-c-market-list__empty-state__body-description"
+          >
+            Error fetching markets
+          </Text>
+        </div>
+        <div className="pm-c-market-list__error__actions">
+          <Button color="primary" size="sm" onClick={handleMarkets}>
+            Try again
+          </Button>
+        </div>
+      </div>
+    );
+
+  if (!markets.length)
+    return (
+      <div className="pm-c-market-list__empty-state">
+        <div className="pm-c-market-list__empty-state__body">
+          <InfoIcon />
+          <Text
+            as="p"
+            scale="tiny"
+            fontWeight="semibold"
+            className="pm-c-market-list__empty-state__body-description"
+          >
+            There are no available markets for this category.
+          </Text>
+        </div>
+      </div>
+    );
+
+  return <MarketList markets={markets} />;
 }
