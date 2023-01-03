9a4cb91c119905da5c0d18e60a2529ecaab44f61
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2023-01-03 11:10:32 -0300

    feat: compose home hero

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,26 +1,15 @@
 import { useCallback, useState } from 'react';
-import { Link } from 'react-router-dom';
 
-import cn from 'classnames';
 import { motion } from 'framer-motion';
-import {
-  Hero,
-  Container,
-  useMedia,
-  ListItem,
-  ListItemText,
-  Adornment
-} from 'ui';
+import { Container, useMedia, ListItem, ListItemText, Adornment } from 'ui';
 
-import heroBanner from 'assets/images/pages/home/illuminate_fantasy_league_banner.png';
-import heroLogo from 'assets/images/pages/home/illuminate_fantasy_league_logo.svg';
-
-import { Icon, MarketListAsync, Text } from 'components';
+import { Icon, MarketListAsync } from 'components';
 import Modal from 'components/Modal';
 import type { ModalProps } from 'components/Modal';
 
 import HomeClasses from './Home.module.scss';
 import HomeFilter from './HomeFilter';
+import HomeHero from './HomeHero';
 import HomeNav from './HomeNav';
 
 function HomeFilterModal(
@@ -69,58 +58,24 @@ export default function Home() {
   return (
     <>
       <Container className={HomeClasses.header}>
-        {isDesktop && (
-          <Hero
-            className={cn('pm-p-home__hero', HomeClasses.hero)}
-            image={heroBanner}
-          >
-            <div className="pm-p-home__hero__content">
-              <div className="pm-p-home__hero__breadcrumb">
-                <Text
-                  as="span"
-                  scale="tiny-uppercase"
-                  fontWeight="semibold"
-                  color="white-50"
-                >
-                  Illuminate Fantasy League / World Cup 2022
-                </Text>
-              </div>
-              <Text
-                as="h2"
-                fontWeight="bold"
-                scale="heading-large"
-                color="light"
-                className="pm-p-home__hero__heading"
-              >
-                Place your World Cup predictions to win the IFL Title!
-              </Text>
-              <Link
-                className="pm-c-button-normal--primary pm-c-button--sm"
-                to="/docs"
-              >
-                About IFL
-              </Link>
-            </div>
-            <img
-              alt="Illuminate Fantasy League"
-              width={293}
-              height={205}
-              src={heroLogo}
-            />
-          </Hero>
-        )}
+        {isDesktop && <HomeHero />}
         <HomeNav
           isDesktop={isDesktop}
           onFilterClick={isDesktop ? handleToggle : handleShow}
         />
       </Container>
       <div
+        className="d-flex"
         style={{
-          display: 'flex',
           height: window.innerHeight
         }}
       >
-        <ModalFilterRoot show={show} onHide={handleHide}>
+        <ModalFilterRoot
+          show={show}
+          {...(!isDesktop && {
+            onHide: handleHide
+          })}
+        >
           <HomeFilter>
             {!isDesktop && (
               <ListItem>
