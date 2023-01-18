7d86b2be9cdfb36c4fcfe432fcb1b676a6d8019c
Wellington Matheus <w.matheus@outlook.com>
2023-01-18 12:20:43 -0300

    feat(Features): add `reportForm` to fantasy

--- src/config/features.ts
+++ src/config/features.ts
@@ -9,7 +9,10 @@ const features = {
     enabled: !toBoolean(environment.FEATURE_FANTASY)
   },
   fantasy: {
-    enabled: toBoolean(environment.FEATURE_FANTASY)
+    enabled: toBoolean(environment.FEATURE_FANTASY),
+    reportForm: {
+      enabled: toBoolean(environment.FEATURE_FANTASY_REPORT_FORM)
+    }
   }
 } as const;
 
