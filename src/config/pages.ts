f03f730af295cfefcf4be29fdd582ad14c8027c2
Wellington Matheus <w.matheus@outlook.com>
2023-01-24 09:35:03 -0300

    feat(PagesConfig): add `enabled` attr

--- src/config/pages.ts
+++ src/config/pages.ts
@@ -1,5 +1,7 @@
 import { lazy } from 'react';
 
+import ui from './ui';
+
 const Leaderboard = lazy(() => import('pages/Leaderboard'));
 const pages = {
   profile: {
@@ -8,7 +10,8 @@ const pages = {
     exact: false,
     navigation: false,
     name: '',
-    meta: null
+    meta: null,
+    enabled: true
   },
   club: {
     pathname: '/clubs/:slug',
@@ -16,7 +19,8 @@ const pages = {
     exact: false,
     navigation: false,
     name: '',
-    meta: null
+    meta: null,
+    enabled: ui.clubs.enabled
   },
   clubs: {
     pathname: '/clubs',
@@ -29,7 +33,8 @@ const pages = {
       description:
         "Build your own Club, league and leaderboard with your friends, against colleagues or around communities. Wear your own logo, tease your clubmates and let all fight to climb the Club's leaderboard.",
       image: `${process.env.PUBLIC_URL}/ifl_meta_clubs.png`
-    }
+    },
+    enabled: ui.clubs.enabled
   },
   leaderboard: {
     pathname: '/leaderboard',
@@ -42,7 +47,8 @@ const pages = {
       description:
         'Rank up higher on the leaderboard and be the #1 forecaster of the Football World Cup. The best global players will earn prizes from the $1500 USD pool, distributed as gift cards.',
       image: `${process.env.PUBLIC_URL}/ifl_meta_leaderboard.png`
-    }
+    },
+    enabled: true
   },
   achievements: {
     pathname: '/achievements',
@@ -56,7 +62,8 @@ const pages = {
       description:
         'Predict Football World Cup match winners and grab your exclusive NFT Achievements. The Illuminate Fantasy League is a fantasy predictions tournament focused on the 2022 Football World Cup.',
       image: `${process.env.PUBLIC_URL}/ifl_meta_achievements.png`
-    }
+    },
+    enabled: true
   },
   portfolio: {
     pathname: '/portfolio',
@@ -69,7 +76,8 @@ const pages = {
       description:
         'Participate in the Illuminate Fantasy League and compete with your friends, coworkers or other community members. Predict Football World Cup match winners and manage your portfolio outcome shares with a seamless and user friendly page.',
       image: `${process.env.PUBLIC_URL}/ifl_meta_portfolio.png`
-    }
+    },
+    enabled: true
   },
   home: {
     pathname: '/',
@@ -83,6 +91,7 @@ const pages = {
         'The Illuminate Fantasy League is a prediction marketplace powered by Polkamarkets, made to celebrate the Football World Cup 2022 with the Moonbeam Community. Join now, bring your friends and start placing your World Cup Predictions for every tournament match to win the IFC title!',
       image: `${process.env.PUBLIC_URL}/ifl_meta.jpg`
     },
+    enabled: true,
     pages: {
       create: {
         pathname: '/markets/create',
@@ -90,7 +99,8 @@ const pages = {
         exact: false,
         navigation: false,
         name: '',
-        meta: null
+        meta: null,
+        enabled: true
       },
       market: {
         pathname: '/markets/:marketId',
@@ -98,7 +108,8 @@ const pages = {
         exact: false,
         navigation: false,
         name: '',
-        meta: null
+        meta: null,
+        enabled: true
       }
     }
   }
