bb2f3cc02e7d2761a46ad42889e14a2ae078c334
Wellington Matheus <w.matheus@outlook.com>
2023-01-25 09:08:56 -0300

    feat(PortfolioTabs): add "Liquidity Positions" and "Report Postions" tabs

--- src/pages/Portfolio/PortfolioTabs.tsx
+++ src/pages/Portfolio/PortfolioTabs.tsx
@@ -1,15 +1,26 @@
 import { useState, useMemo, memo } from 'react';
 
+import { ui } from 'config';
 import { isEmpty } from 'lodash';
 import { setFilter } from 'redux/ducks/portfolio';
 import { useGetMarketsByIdsQuery } from 'services/Polkamarkets';
 import { useMedia } from 'ui';
 
-import { ButtonGroup, PortfolioMarketTable, Filter } from 'components';
+import {
+  ButtonGroup,
+  PortfolioLiquidityTable,
+  PortfolioMarketTable,
+  PortfolioReportTable,
+  Filter
+} from 'components';
 
 import { useAppSelector, useAppDispatch, useNetwork } from 'hooks';
 
-import { formatMarketPositions } from './utils';
+import {
+  formatLiquidityPositions,
+  formatMarketPositions,
+  formatReportPositions
+} from './utils';
 
 function TabsFilter() {
   const dispatch = useAppDispatch();
@@ -55,6 +66,7 @@ function PortfolioTabs() {
   const [currentTab, setCurrentTab] = useState('marketPositions');
   const isDesktop = useMedia('(min-width: 1024px)');
   const {
+    bonds,
     portfolio,
     actions,
     marketsWithActions,
@@ -62,8 +74,11 @@ function PortfolioTabs() {
     isLoading
   } = useAppSelector(state => state.polkamarkets);
 
-  const { portfolio: isLoadingPortfolio, actions: isLoadingActions } =
-    isLoading;
+  const {
+    portfolio: isLoadingPortfolio,
+    bonds: isLoadingBonds,
+    actions: isLoadingActions
+  } = isLoading;
 
   const marketsIds = [...marketsWithActions, ...marketsWithBonds];
 
@@ -74,13 +89,34 @@ function PortfolioTabs() {
         networkId: network.id
       },
       {
-        skip: isLoadingActions || isEmpty(marketsIds)
+        skip:
+          (ui.portfolio.tabs.reportPositions.enabled && isLoadingBonds) ||
+          isLoadingActions ||
+          isEmpty(marketsIds)
       }
     );
+
   const marketPositions = useMemo(
     () => formatMarketPositions(portfolio, actions, markets),
     [actions, markets, portfolio]
   );
+
+  const liquidityPositions = useMemo(() => {
+    if (ui.portfolio.tabs.liquidityPositions.enabled) {
+      return formatLiquidityPositions(portfolio, markets);
+    }
+
+    return undefined;
+  }, [markets, portfolio]);
+
+  const reportPositions = useMemo(() => {
+    if (ui.portfolio.tabs.reportPositions.enabled) {
+      return formatReportPositions(bonds, markets);
+    }
+
+    return undefined;
+  }, [bonds, markets]);
+
   const positions = isDesktop
     ? marketPositions
     : getDefaultCols(marketPositions);
@@ -94,7 +130,20 @@ function PortfolioTabs() {
             {
               id: 'marketPositions',
               name: 'Market Positions',
-              color: 'default'
+              color: 'default',
+              enabled: true
+            },
+            {
+              id: 'liquidityPositions',
+              name: 'Liquidity Positions',
+              color: 'default',
+              enabled: ui.portfolio.tabs.liquidityPositions.enabled
+            },
+            {
+              id: 'reportPositions',
+              name: 'Reports',
+              color: 'default',
+              enabled: ui.portfolio.tabs.reportPositions.enabled
             }
           ]}
           onChange={setCurrentTab}
@@ -109,6 +158,22 @@ function PortfolioTabs() {
             headers={positions.headers}
             isLoadingData={
               isLoadingMarkets || isLoadingPortfolio || isLoadingActions
+            }
+          />
+        ) : null}
+        {liquidityPositions && currentTab === 'liquidityPositions' ? (
+          <PortfolioLiquidityTable
+            rows={liquidityPositions.rows}
+            headers={liquidityPositions.headers}
+            isLoadingData={isLoadingMarkets || isLoadingPortfolio}
+          />
+        ) : null}
+        {reportPositions && currentTab === 'reportPositions' ? (
+          <PortfolioReportTable
+            rows={reportPositions.rows}
+            headers={reportPositions.headers}
+            isLoadingData={
+              isLoadingMarkets || isLoadingPortfolio || isLoadingBonds
             }
           />
         ) : null}
