caa0a8ff3b6af96adbc399ed1f95f69f04391044
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2023-01-29 20:01:31 -0300

    feat: adjust useExpandableOutcomes

--- src/hooks/useExpandableOutcomes/useExpandableOutcomes.ts
+++ src/hooks/useExpandableOutcomes/useExpandableOutcomes.ts
@@ -1,84 +1,49 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 
-import { fromPriceChartToLineChartSeries } from 'helpers/chart';
-import { roundNumber } from 'helpers/math';
-import type { Outcome, PriceChart } from 'models/market';
+import sortOutcomes from 'helpers/sortOutcomes';
+import type { Outcome } from 'models/market';
 
 type UseExpandableOutcomes = {
   max?: number;
-  rawOutcomes: Outcome[];
-};
-
-type SortOutcomes = {
-  timeframe: string;
   outcomes: Outcome[];
 };
 
-function getPricesDiff(priceChart?: PriceChart) {
-  const pricesArr = priceChart?.prices;
-
-  if (!pricesArr)
-    return {
-      value: '0',
-      pct: '0%'
-    };
-
-  const [initial, ...prices] = pricesArr;
-  const diffValue = prices[prices.length - 1].value - initial.value;
-  const diffSign = Math.sign(diffValue) < 0 ? '-' : '+';
-
-  return {
-    value: `${diffSign}${roundNumber(diffValue, 3)}`,
-    pct: `${diffSign}${roundNumber(
-      Math.abs(priceChart.changePercent || 0) * 100,
-      2
-    )}%`
-  } as const;
-}
-function sortOutcomes(args: SortOutcomes) {
-  return [...args.outcomes]
-    .sort((compareA, compareB) => compareB.price - compareA.price)
-    .map(outcome => {
-      const priceChart = outcome.priceCharts?.find(
-        chart => chart.timeframe === args.timeframe
-      );
-
-      return {
-        ...outcome,
-        pricesDiff: getPricesDiff(priceChart),
-        name: outcome.title,
-        data: fromPriceChartToLineChartSeries(priceChart?.prices || [])
-      };
-    });
-}
-
 export default function useExpandableOutcomes({
   max = 2,
-  rawOutcomes
+  outcomes
 }: UseExpandableOutcomes) {
-  const [isExpanded, setExpanded] = useState(rawOutcomes.length < 3);
+  const [isExpanded, setExpanded] = useState(outcomes.length < 3);
   const expand = useCallback(() => setExpanded(true), []);
-  const sorted = sortOutcomes({
-    outcomes: rawOutcomes,
-    timeframe: '7d'
-  });
-  const onseted = sorted.slice(0, isExpanded ? undefined : max);
-  const offseted = sorted.slice(isExpanded ? undefined : max);
-  const isSingle = offseted.length === 1;
+  const sorted = useMemo(
+    () =>
+      sortOutcomes({
+        outcomes,
+        timeframe: '7d'
+      }),
+    [outcomes]
+  );
+  const seted = useMemo(
+    () => ({
+      on: sorted.slice(0, isExpanded ? undefined : max),
+      off: sorted.slice(isExpanded ? undefined : max)
+    }),
+    [isExpanded, max, sorted]
+  );
+  const isSingle = seted.off.length === 1;
 
   return {
     isExpanded,
     expand,
-    onseted,
+    onseted: seted.on,
     offseted: {
       percent:
         +(
-          offseted
+          seted.off
             .map(outcome => outcome.price)
-            .reduce((prices, price) => price + prices, 0) / offseted.length
+            .reduce((prices, price) => price + prices, 0) / seted.off.length
         ).toFixed(3) * 100,
-      primary: `+${offseted.length} Outcome${isSingle ? '' : 's'}`,
-      secondary: `${offseted
+      primary: `+${seted.off.length} Outcome${isSingle ? '' : 's'}`,
+      secondary: `${seted.off
         .slice(0, 2)
         .map(outcome => outcome.name)
         .join(', ')}${isSingle ? '' : '...'}`
