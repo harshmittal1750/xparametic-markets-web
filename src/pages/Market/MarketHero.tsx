dacb0dc6fdafe3bad41089cc6dc0c5540263088f
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2023-02-01 10:54:18 -0300

    feat: adjust res UI for MarketHero

--- src/pages/Market/MarketHero.tsx
+++ src/pages/Market/MarketHero.tsx
@@ -1,4 +1,6 @@
-import { Container, Hero } from 'ui';
+import { Fragment } from 'react';
+
+import { Container, Hero, useMedia } from 'ui';
 import Avatar from 'ui/Avatar';
 
 import { Breadcrumb, Text } from 'components';
@@ -9,13 +11,29 @@ import { useAppSelector } from 'hooks';
 
 import marketClasses from './Market.module.scss';
 
-export default function MarketHero() {
+function MarketHeroWrapper(
+  props: React.PropsWithChildren<Record<string, unknown>>
+) {
   const market = useAppSelector(state => state.market.market);
 
   return (
-    <Hero className={marketClasses.hero} $image={market.imageUrl}>
-      <Container className={marketClasses.heroInfo}>
-        <Avatar $size="lg" $radius="lg" alt="Market" src={market.imageUrl} />
+    <Hero className={marketClasses.hero} $image={market.imageUrl} {...props} />
+  );
+}
+export default function MarketHero() {
+  const market = useAppSelector(state => state.market.market);
+  const isDesktop = useMedia('(min-width: 1024px)');
+  const MarketHeroWrapperComponent = isDesktop ? MarketHeroWrapper : Fragment;
+
+  return (
+    <MarketHeroWrapperComponent>
+      <Container $enableGutters={!isDesktop} className={marketClasses.heroInfo}>
+        <Avatar
+          $size={isDesktop ? 'lg' : 'md'}
+          $radius="lg"
+          alt="Market"
+          src={market.imageUrl}
+        />
         <div>
           <Breadcrumb>
             <Breadcrumb.Item>{market.category}</Breadcrumb.Item>
@@ -23,20 +41,24 @@ export default function MarketHero() {
           </Breadcrumb>
           <Text
             as="h2"
-            fontWeight="bold"
-            scale="heading-large"
+            fontWeight={isDesktop ? 'bold' : 'medium'}
+            scale={isDesktop ? 'heading-large' : 'body'}
             style={{ color: 'var(--color-text-primary)' }}
           >
             {market.title}
           </Text>
         </div>
-        <div className={marketClasses.heroInfoActions}>
-          <MarketFooterActions $variant="filled" market={market} />
-        </div>
+        {isDesktop && (
+          <div className={marketClasses.heroInfoActions}>
+            <MarketFooterActions $variant="filled" market={market} />
+          </div>
+        )}
       </Container>
-      <Container className={marketClasses.heroStats}>
-        <MarketFooter market={market} />
-      </Container>
-    </Hero>
+      {isDesktop && (
+        <Container className={marketClasses.heroStats}>
+          <MarketFooter market={market} />
+        </Container>
+      )}
+    </MarketHeroWrapperComponent>
   );
 }
