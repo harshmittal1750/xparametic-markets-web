b2a6022b104083ce8279835ff8e8b97791c0d397
Wellington Matheus <w.matheus@outlook.com>
2023-02-08 11:37:01 -0300

    feat(Config): create tokens config

---
+++ src/config/tokens.ts
@@ -0,0 +1,11 @@
+import type { Token } from 'types/token';
+
+const USDC: Token = {
+  name: 'USD Coin',
+  ticker: 'USDC',
+  symbol: 'USDC',
+  iconName: 'USDCoin',
+  addresses: {}
+};
+
+export default { USDC };
