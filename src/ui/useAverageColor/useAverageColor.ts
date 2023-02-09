20c278be165dcff70596dc97bc4b285591500a7d
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2023-02-09 15:20:01 -0300

    feat: create useAverageColor

---
+++ src/ui/useAverageColor/useAverageColor.ts
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+
+export default function useAverageColor(
+  ref: React.RefObject<HTMLImageElement>
+) {
+  const [RGB, setRGBA] = useState({
+    red: 0,
+    green: 0,
+    blue: 0
+  });
+  const context = document.createElement('canvas').getContext?.('2d');
+
+  useEffect(() => {
+    const { current: node } = ref;
+
+    async function buildImage() {
+      if (node) {
+        node.crossOrigin = 'Anonymous';
+
+        try {
+          context?.drawImage(node, 0, 0, 1, 1);
+          const imageData = context?.getImageData(0, 0, 1, 1);
+
+          if (imageData?.data) {
+            setRGBA({
+              red: imageData?.data[0],
+              green: imageData?.data[1],
+              blue: imageData?.data[2]
+            });
+          }
+        } catch (error) {
+          // eslint-disable-next-line no-console
+          if (error instanceof Error) console.error(new Error(error.message));
+        }
+      }
+    }
+
+    node?.addEventListener('load', buildImage);
+
+    return () => node?.removeEventListener('load', buildImage);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [ref]);
+
+  return RGB;
+}
