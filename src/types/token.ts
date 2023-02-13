cb10f74185bc0a2582d4f052059dca7f483b7f20
Wellington Matheus <w.matheus@outlook.com>
2023-02-13 01:30:57 -0300

    feat(TokenTypes): add `Tether` to `iconName` union

--- src/types/token.ts
+++ src/types/token.ts
@@ -4,7 +4,7 @@ export type Token = {
   name: string;
   ticker: string;
   symbol: string;
-  iconName: Extract<IconProps['name'], 'USDCoin'>;
+  iconName: Extract<IconProps['name'], 'USDCoin' | 'Tether'>;
   addresses: {
     [key: string]: `0x${string}`;
   };
