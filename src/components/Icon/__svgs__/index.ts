075df1769c51635a853d78725a2e770809dae1e8
Wellington Matheus <w.matheus@outlook.com>
2023-02-13 01:37:46 -0300

    feat(Icon): add `Polk` svg

--- src/components/Icon/__svgs__/index.ts
+++ src/components/Icon/__svgs__/index.ts
@@ -28,6 +28,7 @@ export { ReactComponent as Moon } from './Moon.svg';
 export { ReactComponent as Moonbeam } from './Moonbeam.svg';
 export { ReactComponent as Moonriver } from './Moonriver.svg';
 export { ReactComponent as Plus } from './Plus.svg';
+export { ReactComponent as Polk } from './Polk.svg';
 export { ReactComponent as Radio } from './Radio.svg';
 export { ReactComponent as RadioFilled } from './RadioFilled.svg';
 export { ReactComponent as Share } from './Share.svg';
