5687559d05ad997ae3944d8b015a39e908cc7373
Wellington Matheus <w.matheus@outlook.com>
2023-02-13 01:24:37 -0300

    feat(TokensConfig): add `USDC` addresses

--- src/config/tokens.ts
+++ src/config/tokens.ts
@@ -5,7 +5,12 @@ const USDC: Token = {
   ticker: 'USDC',
   symbol: 'USDC',
   iconName: 'USDCoin',
-  addresses: {}
+  addresses: {
+    Moonriver: '0xe3f5a90f9cb311505cd691a46596599aa1a0ad7d',
+    Moonbeam: '0x8f552a71efe5eefc207bf75485b356a0b3f01ec9',
+    Goerli: '0x07865c6e87b9f70255377e024ace6630c1eaa37f',
+    Polygon: '0x2791bca1f2de4661ed88a30c99a7a9449aa84174'
+  }
 };
 
 export default { USDC };
