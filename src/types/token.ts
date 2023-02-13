4ef90469a3d9ca2c19f3a166ac0a98186988e72c
Wellington Matheus <w.matheus@outlook.com>
2023-02-13 01:38:02 -0300

    feat(TokenTypes): add `Polk` to `iconName` union

--- src/types/token.ts
+++ src/types/token.ts
@@ -4,7 +4,7 @@ export type Token = {
   name: string;
   ticker: string;
   symbol: string;
-  iconName: Extract<IconProps['name'], 'USDCoin' | 'Tether'>;
+  iconName: Extract<IconProps['name'], 'USDCoin' | 'Tether' | 'Polk'>;
   addresses: {
     [key: string]: `0x${string}`;
   };
