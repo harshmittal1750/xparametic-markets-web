e87e35938cda4bb1012c16080f4d7fd2d9c9b5a3
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2023-02-23 21:31:12 -0300

    feat: use SelectTokenModal as adornment

--- src/components/CreateMarketForm/CreateMarketFormFund.tsx
+++ src/components/CreateMarketForm/CreateMarketFormFund.tsx
@@ -16,76 +16,122 @@ import Text from '../Text';
 import Tooltip from '../Tooltip';
 
 function CreateMarketFormFund() {
-  const {
-    network: { currency },
-    networkConfig
-  } = useNetwork();
+  const network = useNetwork();
+  const balance = useAppSelector(state => state.polkamarkets.ethBalance);
   const [field, _meta, helpers] = useField('liquidity');
   const [fee, setFee] = useState(0);
 
-  const balance = useAppSelector(state => state.polkamarkets.ethBalance);
-
-  function handleChangeAmount(amount: number) {
-    helpers.setValue(amount);
-  }
-
   useEffect(() => {
     (async function getMarketFee() {
-      const polkamarketsService = new PolkamarketsService(networkConfig);
+      const polkamarketsService = new PolkamarketsService(
+        network.networkConfig
+      );
 
       const response = await polkamarketsService.getMarketFee();
       setFee(response);
     })();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+  function handleChangeAmount(amount: number) {
+    helpers.setValue(amount);
+  }
 
   return (
-    <div className="pm-c-create-market-form__card">
-      <Text
-        as="h5"
-        scale="body"
-        fontWeight="medium"
-        className="pm-c-create-market-form__card-title"
-      >
-        Fund Market
-      </Text>
-      <AlertMini
-        variant="information"
-        styles="outline"
-        description={
-          <>
-            {`Providing liquidity is risky and can lead to near total loss. Market prices can move abruptly at any time. `}
-            <Link
-              title="More Info"
-              href="//help.polkamarkets.com/en/articles/6153227-strategies-and-risks-for-liquidity-providers"
-              aria-label="More Info"
-              target="_blank"
-              rel="noreferrer"
-              variant="information"
-              scale="tiny"
-              fontWeight="medium"
-            />
-          </>
-        }
-      />
-      <SelectTokenModal />
-      <AmountInput
-        label="Add Liquidity"
-        max={roundDown(balance)}
-        currency={currency}
-        onChange={handleChangeAmount}
-      />
-      <div className="pm-c-create-market-form__card-liquidity-details">
-        <div className="pm-c-create-market-form__card-liquidity-details__group">
-          <div className="pm-c-create-market-form__card-liquidity-details__liquidity-value">
+    <>
+      <div className="pm-c-create-market-form__card">
+        <Text
+          as="h5"
+          scale="body"
+          fontWeight="medium"
+          className="pm-c-create-market-form__card-title"
+        >
+          Fund Market
+        </Text>
+        <AlertMini
+          variant="information"
+          styles="outline"
+          description={
+            <>
+              {`Providing liquidity is risky and can lead to near total loss. Market prices can move abruptly at any time. `}
+              <Link
+                title="More Info"
+                href="//help.polkamarkets.com/en/articles/6153227-strategies-and-risks-for-liquidity-providers"
+                aria-label="More Info"
+                target="_blank"
+                rel="noreferrer"
+                variant="information"
+                scale="tiny"
+                fontWeight="medium"
+              />
+            </>
+          }
+        />
+        <AmountInput
+          label="Add Liquidity"
+          max={roundDown(balance)}
+          currency={network.network.currency}
+          onChange={handleChangeAmount}
+          endAdornment={<SelectTokenModal network={network.network} />}
+        />
+        <div className="pm-c-create-market-form__card-liquidity-details">
+          <div className="pm-c-create-market-form__card-liquidity-details__group">
+            <div className="pm-c-create-market-form__card-liquidity-details__liquidity-value">
+              <Text
+                as="span"
+                scale="caption"
+                fontWeight="semibold"
+                className="pm-c-create-market-form__card-liquidity-details__liquidity-value__title"
+              >
+                Liquidity Value
+                <Tooltip text="Amount added to liquidity pool" position="top">
+                  <InfoIcon />
+                </Tooltip>
+              </Text>
+              <Text
+                as="span"
+                scale="caption"
+                fontWeight="semibold"
+                className="pm-c-create-market-form__card-liquidity-details__liquidity-value__amount"
+              >
+                {`${roundNumber(field.value, 3)} ${
+                  network.network.currency.symbol
+                }`}
+              </Text>
+            </div>
+            <div className="pm-c-create-market-form__card-liquidity-details__shares-added">
+              <Text
+                as="span"
+                scale="tiny"
+                fontWeight="semibold"
+                className="pm-c-create-market-form__card-liquidity-details__shares-added__title"
+              >
+                Est. Liquidity Shares Added
+              </Text>
+
+              <Text
+                as="span"
+                scale="tiny"
+                fontWeight="semibold"
+                className="pm-c-create-market-form__card-liquidity-details__shares-added__amount"
+              >
+                {roundNumber(field.value, 3)}
+              </Text>
+            </div>
+          </div>
+          <hr className="pm-c-create-market-form__card-liquidity-details__separator" />
+
+          <div className="pm-c-create-market-form__card-liquidity-details__earn-trading-fee">
             <Text
               as="span"
               scale="caption"
               fontWeight="semibold"
-              className="pm-c-create-market-form__card-liquidity-details__liquidity-value__title"
+              className="pm-c-create-market-form__card-liquidity-details__earn-trading-fee__title"
             >
-              Liquidity Value
-              <Tooltip text="Amount added to liquidity pool" position="top">
+              Trading Fee Earnings
+              <Tooltip
+                text="Fee given to liquidity providers on every buy/sell transaction"
+                position="top"
+              >
                 <InfoIcon />
               </Tooltip>
             </Text>
@@ -94,60 +140,14 @@ function CreateMarketFormFund() {
               as="span"
               scale="caption"
               fontWeight="semibold"
-              className="pm-c-create-market-form__card-liquidity-details__liquidity-value__amount"
+              className="pm-c-create-market-form__card-liquidity-details__earn-trading-fee__amount"
             >
-              {`${roundNumber(field.value, 3)} ${currency.symbol}`}
+              {`${roundNumber(fee * 100, 3)} %`}
             </Text>
           </div>
-          <div className="pm-c-create-market-form__card-liquidity-details__shares-added">
-            <Text
-              as="span"
-              scale="tiny"
-              fontWeight="semibold"
-              className="pm-c-create-market-form__card-liquidity-details__shares-added__title"
-            >
-              Est. Liquidity Shares Added
-            </Text>
-
-            <Text
-              as="span"
-              scale="tiny"
-              fontWeight="semibold"
-              className="pm-c-create-market-form__card-liquidity-details__shares-added__amount"
-            >
-              {roundNumber(field.value, 3)}
-            </Text>
-          </div>
-        </div>
-        <hr className="pm-c-create-market-form__card-liquidity-details__separator" />
-
-        <div className="pm-c-create-market-form__card-liquidity-details__earn-trading-fee">
-          <Text
-            as="span"
-            scale="caption"
-            fontWeight="semibold"
-            className="pm-c-create-market-form__card-liquidity-details__earn-trading-fee__title"
-          >
-            Trading Fee Earnings
-            <Tooltip
-              text="Fee given to liquidity providers on every buy/sell transaction"
-              position="top"
-            >
-              <InfoIcon />
-            </Tooltip>
-          </Text>
-
-          <Text
-            as="span"
-            scale="caption"
-            fontWeight="semibold"
-            className="pm-c-create-market-form__card-liquidity-details__earn-trading-fee__amount"
-          >
-            {`${roundNumber(fee * 100, 3)} %`}
-          </Text>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
