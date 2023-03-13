85a75e378b1e37d00a4933e7132bc0e2f718ac41
Wellington Matheus <w.matheus@outlook.com>
2023-03-13 07:47:23 -0300

    refactor(Toggle): implement `forwardRef` pattern

--- src/ui/Toggle/Toggle.tsx
+++ src/ui/Toggle/Toggle.tsx
@@ -1,3 +1,5 @@
+import { forwardRef } from 'react';
+
 import cn from 'classnames';
 
 import { Icon } from 'components';
@@ -5,25 +7,37 @@ import { Icon } from 'components';
 import ToggleClasses from './Toggle.module.scss';
 
 type ReactInputProps = React.ComponentPropsWithoutRef<'input'>;
-interface ToggleProps extends Omit<ReactInputProps, 'type'> {
+
+type Ref = HTMLInputElement;
+
+type ToggleProps = Omit<ReactInputProps, 'type'> & {
   type: Extract<ReactInputProps['type'], 'radio' | 'checkbox'>;
-}
+};
 
-export default function Toggle(props: ToggleProps) {
-  const { checked, type } = props;
-  const renderIconName = {
-    checkbox: checked ? 'CheckboxFilled' : 'Checkbox',
-    radio: checked ? 'RadioFilled' : 'Radio'
-  } as const;
+const Toggle = forwardRef<Ref, ToggleProps>(
+  ({ type, checked, ...props }, ref) => {
+    const renderIconName = {
+      checkbox: checked ? 'CheckboxFilled' : 'Checkbox',
+      radio: checked ? 'RadioFilled' : 'Radio'
+    } as const;
 
-  return (
-    <span
-      className={cn(ToggleClasses.root, {
-        [ToggleClasses.checked]: checked
-      })}
-    >
-      <input className={ToggleClasses.input} {...props} />
-      <Icon className={ToggleClasses.icon} name={renderIconName[type]} />
-    </span>
-  );
-}
+    return (
+      <span
+        className={cn(ToggleClasses.root, {
+          [ToggleClasses.checked]: checked
+        })}
+      >
+        <input
+          ref={ref}
+          type={type}
+          checked={checked}
+          className={ToggleClasses.input}
+          {...props}
+        />
+        <Icon className={ToggleClasses.icon} name={renderIconName[type]} />
+      </span>
+    );
+  }
+);
+
+export default Toggle;
