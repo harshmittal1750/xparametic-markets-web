d921476c94dcf9647f18551830a4f28970f42683
soujvnunes <soujvnunes@gmail.com>
2023-04-03 12:37:38 -0300

    fat: adjust useUpdateEffect legibility

--- src/ui/useUpdateEffect/useUpdateEffect.ts
+++ src/ui/useUpdateEffect/useUpdateEffect.ts
@@ -1,14 +1,16 @@
 import { useLayoutEffect, useRef } from 'react';
 
+/**
+ * Fires the effect callback just after updates. Has the same signature as `useLayoutEffect`.
+ */
 export default function useUpdateEffect(
   ...args: Parameters<typeof useLayoutEffect>
 ) {
-  const didMount = useRef(true);
+  const isMountPhase = useRef(true);
 
   useLayoutEffect(() => {
-    if (didMount.current) {
-      didMount.current = false;
-    } else args[0]();
+    if (isMountPhase.current) isMountPhase.current = false;
+    else args[0]();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, args[1]);
+  }, args?.[1]);
 }
