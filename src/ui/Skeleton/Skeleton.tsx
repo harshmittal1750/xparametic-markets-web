1280b4bab74ea0dd08378eb24577173daf331033
soujvnunes <soujvnunes@gmail.com>
2023-04-05 15:50:43 -0300

    feat: extend Skeleton styles

--- src/ui/Skeleton/Skeleton.tsx
+++ src/ui/Skeleton/Skeleton.tsx
@@ -1,5 +1,21 @@
+import type { ComponentPropsWithoutRef } from 'react';
+
+import cn from 'classnames';
+
 import skeletonClasses from './Skeleton.module.scss';
 
-export default function Skeleton() {
-  return <span role="alert" aria-busy className={skeletonClasses.root} />;
+type SkeletonProps = Pick<
+  ComponentPropsWithoutRef<'span'>,
+  'className' | 'style'
+>;
+
+export default function Skeleton({ className, ...props }: SkeletonProps) {
+  return (
+    <span
+      role="alert"
+      aria-busy
+      className={cn(skeletonClasses.root, className)}
+      {...props}
+    />
+  );
 }
