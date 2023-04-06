d8132dfbb142738133478cf68bcd04b45cf5a3f5
Wellington Matheus <w.matheus@outlook.com>
2023-04-06 00:53:47 -0300

    feat(CreateMarket): create heading and subheading

--- src/pages/CreateMarket/CreateMarket.tsx
+++ src/pages/CreateMarket/CreateMarket.tsx
@@ -3,10 +3,11 @@ import { useEffect, useState } from 'react';
 import { PolkamarketsService } from 'services';
 import { Container } from 'ui';
 
-import { Text, CreateMarketForm } from 'components';
+import { CreateMarketForm } from 'components';
 
 import { useAppSelector, useNetwork } from 'hooks';
 
+import CreateMarketClasses from './CreateMarket.module.scss';
 import CreateMarketBuyPolk from './CreateMarketBuyPolk';
 
 function CreateMarket() {
@@ -26,20 +27,26 @@ function CreateMarket() {
   }, [polkBalance, networkConfig]);
 
   return (
-    <Container $size="sm" $enableGutters className="pm-p-create-market">
+    <Container $size="sm" className={CreateMarketClasses.root}>
       {needsBuyPolk && (
         <div className="pm-p-create-market__notification-overlay">
           <CreateMarketBuyPolk requiredPolkBalance={requiredBalance} />
         </div>
       )}
-      <Text
-        as="h4"
-        scale="heading"
-        fontWeight="semibold"
-        className="pm-p-create-market__header-title"
-      >
-        Create New Market
-      </Text>
+      <h2 className={CreateMarketClasses.heading}>
+        Create Your Forecasting Market
+      </h2>
+      <h3 className={CreateMarketClasses.subheading}>
+        Set up a new forecasting market by providing the necessary details and
+        funding information. Follow the step-by-step process to create a market
+        that&apos;s engaging and easy to participate in for users.{' '}
+        <a
+          href="help.polkamarkets.com"
+          className={CreateMarketClasses.subheadingLink}
+        >
+          Learn more
+        </a>
+      </h3>
       <CreateMarketForm />
     </Container>
   );
