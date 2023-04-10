ba9cc64076c6fe45dde87b6038bd04ebf6958d3f
Wellington Matheus <w.matheus@outlook.com>
2023-04-10 04:21:02 -0300

    feat(Components): export `CreateMarketFormOutcomes`

--- src/components/index.ts
+++ src/components/index.ts
@@ -154,6 +154,8 @@ export { default as CreateMarket } from './CreateMarket';
 
 export { default as CreateMarketFormDetails } from './CreateMarketFormDetails';
 
+export { default as CreateMarketFormOutcomes } from './CreateMarketFormOutcomes';
+
 export { default as Icon, IconClasses } from './Icon';
 export type { IconProps } from './Icon';
 
