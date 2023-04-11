2ef4261cb5dfbf6bd9a301090502879fcb1b695e
Wellington Matheus <w.matheus@outlook.com>
2023-04-11 11:28:42 -0300

    feat(Input): add `required` optional prop

--- src/components/Input/SelectInput.tsx
+++ src/components/Input/SelectInput.tsx
@@ -16,55 +16,71 @@ type SelectInputProps = {
   name: string;
   description?: string;
   options: SelectInputOption[];
+  required?: boolean;
 };
 
 const SelectInput = React.forwardRef<
   HTMLSelectElement,
   SelectInputProps & React.SelectHTMLAttributes<HTMLSelectElement>
->(({ label, name, description, options, placeholder, ...props }, ref) => {
-  const [field, meta] = useField(name);
+>(
+  (
+    {
+      label,
+      name,
+      description,
+      options,
+      placeholder,
+      required = false,
+      ...props
+    },
+    ref
+  ) => {
+    const [field, meta] = useField(name);
 
-  const hasError = meta.touched && meta.error;
+    const hasError = meta.touched && meta.error;
 
-  return (
-    <div className="pm-c-select-input__group">
-      <label
-        htmlFor={name}
-        className={`pm-c-select-input__label--${
-          hasError ? 'error' : 'default'
-        }`}
-      >
-        {label}
-      </label>
-      <select
-        ref={ref}
-        id={name}
-        {...field}
-        {...props}
-        className={cn({
-          'pm-c-input--error': hasError,
-          'pm-c-input--default': !hasError,
-          'pm-c-select-input--empty': !field.value
-        })}
-      >
-        <option value="" disabled selected>
-          {placeholder}
-        </option>
-        {options.map(option => (
-          <option key={option.value} value={option.value}>
-            {option.name}
+    return (
+      <div className="pm-c-select-input__group">
+        <label
+          htmlFor={name}
+          className={cn({
+            'pm-c-select-input__label--error': hasError,
+            'pm-c-select-input__label--default': !hasError,
+            'pm-c-select-input__label--required': required
+          })}
+        >
+          {label}
+        </label>
+        <select
+          ref={ref}
+          id={name}
+          {...field}
+          {...props}
+          className={cn({
+            'pm-c-input--error': hasError,
+            'pm-c-input--default': !hasError,
+            'pm-c-select-input--empty': !field.value
+          })}
+        >
+          <option value="" disabled selected>
+            {placeholder}
           </option>
-        ))}
-      </select>
-      {hasError && meta.error ? (
-        <InputErrorMessage message={meta.error} />
-      ) : null}
-      {description && !hasError ? (
-        <span className="pm-c-input__description">{description}</span>
-      ) : null}
-    </div>
-  );
-});
+          {options.map(option => (
+            <option key={option.value} value={option.value}>
+              {option.name}
+            </option>
+          ))}
+        </select>
+        {hasError && meta.error ? (
+          <InputErrorMessage message={meta.error} />
+        ) : null}
+        {description && !hasError ? (
+          <span className="pm-c-input__description">{description}</span>
+        ) : null}
+      </div>
+    );
+  }
+);
 
 SelectInput.displayName = 'SelectInput';
 
