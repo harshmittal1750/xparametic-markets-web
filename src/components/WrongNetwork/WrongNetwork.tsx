ad9987b99561bf66be154a75f30fc07a2b2a77ef
soujvnunes <soujvnunes@gmail.com>
2023-04-14 19:43:11 -0300

    feat: adjust r8e for WrongNetwork

--- src/components/WrongNetwork/WrongNetwork.tsx
+++ src/components/WrongNetwork/WrongNetwork.tsx
@@ -1,27 +1,33 @@
 import type { Network } from 'types/network';
-import { Alert } from 'ui';
+import { Alert, useTheme } from 'ui';
 
 import { Button, Text } from 'components';
+
+import wrongNetwork from './WrongNetwork.module.scss';
 
 type WrongNetworkProps = {
   network: Network;
 };
 
 export default function WrongNetwork({ network }: WrongNetworkProps) {
+  const theme = useTheme();
+
   return (
-    <Alert $type="warning" $variant="subtle">
-      <Text fontWeight="medium" scale="tiny">
+    <Alert
+      $type="warning"
+      $variant="subtle"
+      $enableGutters={!theme.device.isTablet}
+    >
+      <Text
+        fontWeight="medium"
+        scale={theme.device.isDesktop ? 'caption' : 'tiny'}
+      >
         The selected network <strong>{network.name}</strong> does not match to
         the one on your wallet. Consider{' '}
         <Button
           variant="ghost"
-          size="xs"
-          style={{
-            padding: 0,
-            color: 'inherit',
-            verticalAlign: 'baseline',
-            textDecoration: 'underline'
-          }}
+          size={theme.device.isDesktop ? 'sm' : 'xs'}
+          className={wrongNetwork.link}
         >
           changing it
         </Button>{' '}
