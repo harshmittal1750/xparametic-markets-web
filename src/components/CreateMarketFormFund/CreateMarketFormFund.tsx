7dafd0955d1f612988d980af48c581707b93dd99
Wellington Matheus <w.matheus@outlook.com>
2023-04-29 13:56:53 -0300

    feat(CreateMarketFormFund): add market preview section

--- src/components/CreateMarketFormFund/CreateMarketFormFund.tsx
+++ src/components/CreateMarketFormFund/CreateMarketFormFund.tsx
@@ -18,6 +18,7 @@ import Text from '../Text';
 import Tooltip from '../Tooltip';
 import AmountInput from './AmountInput';
 import CreateMarketFormFundClasses from './CreateMarketFormFund.module.scss';
+import MarketPreview from './MarketPreview';
 
 function CreateMarketFormFund() {
   const network = useNetwork();
@@ -167,7 +168,6 @@ function CreateMarketFormFund() {
             </div>
           </div>
           <hr className="pm-c-create-market-form__card-liquidity-details__separator" />
-
           <div
             className={cn(
               'pm-c-create-market-form__card-liquidity-details__earn-trading-fee',
@@ -192,12 +192,13 @@ function CreateMarketFormFund() {
           </div>
         </div>
       </div>
-      {/* <div className={CreateMarketFormFundClasses.append}>
+      <div className={CreateMarketFormFundClasses.append}>
         <h4 className={CreateMarketFormFundClasses.appendTitle}>
           Great! Your market is almost ready. Please review the details of your
           forecasting market below and confirm its accuracy before submitting.
         </h4>
-      </div> */}
+        <MarketPreview token={currency} />
+      </div>
     </>
   );
 }
