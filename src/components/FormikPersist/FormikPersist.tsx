42779cbfe24985bbb6b0e2df7d37f40a6981d8cc
Wellington Matheus <w.matheus@outlook.com>
2023-04-29 10:00:54 -0300

    feat(FormikPersist): add `currentStep`

--- src/components/FormikPersist/FormikPersist.tsx
+++ src/components/FormikPersist/FormikPersist.tsx
@@ -6,12 +6,21 @@ import isEqual from 'lodash/isEqual';
 
 import type { CreateMarketFormData } from '../CreateMarketForm';
 
-function FormikPersist() {
+type FormikPersistProps = {
+  currentStep: number;
+  onChangeCurrentStep: (step: number) => void;
+};
+
+function FormikPersist({
+  currentStep,
+  onChangeCurrentStep
+}: FormikPersistProps) {
   const { values, setValues, touched, setTouched } =
     useFormikContext<CreateMarketFormData>();
 
   const valuesRef = useRef<CreateMarketFormData>();
   const touchedValuesRef = useRef<FormikTouched<CreateMarketFormData>>();
+  const currentStepRef = useRef<number>();
 
   const onSaveValues = async () => {
     window.localStorage.setItem('createMarketValues', JSON.stringify(values));
@@ -21,8 +30,16 @@ function FormikPersist() {
     window.localStorage.setItem('createMarketTouched', JSON.stringify(touched));
   };
 
+  const onSaveCurrentStep = () => {
+    window.localStorage.setItem(
+      'createMarketCurrentStep',
+      currentStep.toString()
+    );
+  };
+
   const debouncedOnSaveValues = debounce(onSaveValues, 300);
   const debouncedOnSaveTouched = debounce(onSaveTouched, 300);
+  const debouncedOnSaveCurrentStep = debounce(onSaveCurrentStep, 300);
 
   useEffect(() => {
     const savedValues = window.localStorage.getItem('createMarketValues');
@@ -49,6 +66,20 @@ function FormikPersist() {
   }, [setTouched]);
 
   useEffect(() => {
+    const savedCurrentStep = window.localStorage.getItem(
+      'createMarketCurrentStep'
+    );
+
+    if (savedCurrentStep) {
+      const parsedCurrentStep = parseInt(savedCurrentStep, 10);
+
+      currentStepRef.current = parsedCurrentStep;
+
+      onChangeCurrentStep(parsedCurrentStep);
+    }
+  }, [onChangeCurrentStep]);
+
+  useEffect(() => {
     if (!isEqual(valuesRef.current, values)) {
       debouncedOnSaveValues();
     }
@@ -61,8 +92,15 @@ function FormikPersist() {
   });
 
   useEffect(() => {
+    if (currentStepRef.current !== currentStep) {
+      debouncedOnSaveCurrentStep();
+    }
+  });
+
+  useEffect(() => {
     valuesRef.current = values;
     touchedValuesRef.current = touched;
+    currentStepRef.current = currentStep;
   });
 
   return null;
