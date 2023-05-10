828f84ed1195a3dcd6d68fa1caa08067e5322007
Wellington Matheus <w.matheus@outlook.com>
2023-05-10 18:33:38 -0300

    feat: add `@babel/plugin-proposal-numeric-separator` plugin

--- craco.config.js
+++ craco.config.js
@@ -33,7 +33,8 @@ module.exports = {
       '@babel/plugin-proposal-class-properties',
       '@babel/plugin-proposal-nullish-coalescing-operator',
       '@babel/plugin-proposal-optional-chaining',
-      '@babel/plugin-proposal-private-methods'
+      '@babel/plugin-proposal-private-methods',
+      '@babel/plugin-proposal-numeric-separator'
     ]
   }
 };
