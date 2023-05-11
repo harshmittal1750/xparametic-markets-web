813038f87ca9a36e04bdd2fae9c5d1df4a740022
soujvnunes <soujvnunes@gmail.com>
2023-05-11 12:02:59 -0300

    feat: add button el for clickable list items

--- src/ui/ListItem/ListItem.tsx
+++ src/ui/ListItem/ListItem.tsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { createElement } from 'react';
 
 import cn from 'classnames';
 
-import ListItemClasses from './ListItem.module.scss';
+import listItemClasses from './ListItem.module.scss';
+
+type ListItemProps = (
+  | ({
+      onClick?: undefined;
+    } & React.ComponentPropsWithoutRef<'li'>)
+  | ({
+      onClick?: React.ComponentPropsWithoutRef<'button'>['onClick'];
+    } & React.ComponentPropsWithoutRef<'button'>)
+) & {
+  $actived?: boolean;
+};
 
 export default function ListItem({
   className,
+  $actived,
   ...props
-}: React.ComponentPropsWithoutRef<'li'>) {
-  return <li className={cn(ListItemClasses.root, className)} {...props} />;
+}: ListItemProps) {
+  const el = props.onClick ? 'button' : 'li';
+  const isButton = el === 'button';
+
+  return createElement(el, {
+    className: cn(
+      listItemClasses.root,
+      {
+        [listItemClasses.button]: isButton,
+        [listItemClasses.buttonActive]: isButton && $actived
+      },
+      className
+    ),
+    ...props
+  });
 }
