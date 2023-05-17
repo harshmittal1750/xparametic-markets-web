b5404928ecfe571c276462910aa84b424a9c989f
soujvnunes <soujvnunes@gmail.com>
2023-05-17 09:23:11 -0300

    feat: wrap list item button

--- src/ui/ListItem/ListItem.tsx
+++ src/ui/ListItem/ListItem.tsx
@@ -1,37 +1,66 @@
-import React, { createElement } from 'react';
+import React from 'react';
 
 import cn from 'classnames';
 
 import listItemClasses from './ListItem.module.scss';
 
-type ListItemProps = (
-  | ({
-      onClick?: undefined;
-    } & React.ComponentPropsWithoutRef<'li'>)
-  | ({
-      onClick?: React.ComponentPropsWithoutRef<'button'>['onClick'];
-    } & React.ComponentPropsWithoutRef<'button'>)
-) & {
+type ListItemProps = Pick<
+  React.ComponentPropsWithoutRef<'li'>,
+  'className' | 'children'
+> & {
   $actived?: boolean;
-};
+} & (
+    | {
+        onClick?: undefined;
+        name?: undefined;
+        ButtonProps?: undefined;
+      }
+    | {
+        onClick?: React.ComponentPropsWithoutRef<'button'>['onClick'];
+        name?: React.ComponentPropsWithoutRef<'button'>['name'];
+        ButtonProps?: React.ComponentPropsWithoutRef<'button'>;
+      }
+  );
 
 export default function ListItem({
   className,
+  children,
+  ButtonProps,
+  name,
+  onClick,
   $actived,
   ...props
 }: ListItemProps) {
-  const el = props.onClick ? 'button' : 'li';
-  const isButton = el === 'button';
-
-  return createElement(el, {
-    className: cn(
-      listItemClasses.root,
-      {
-        [listItemClasses.button]: isButton,
-        [listItemClasses.buttonActive]: isButton && $actived
-      },
-      className
-    ),
-    ...props
-  });
+  return (
+    <li
+      className={cn(
+        {
+          [listItemClasses.root]: !onClick
+        },
+        className
+      )}
+      {...props}
+    >
+      {onClick ? (
+        <button
+          type="button"
+          className={cn(
+            listItemClasses.root,
+            listItemClasses.button,
+            {
+              [listItemClasses.buttonActive]: $actived
+            },
+            ButtonProps?.className
+          )}
+          onClick={onClick}
+          name={name}
+          {...ButtonProps}
+        >
+          {children}
+        </button>
+      ) : (
+        children
+      )}
+    </li>
+  );
 }
