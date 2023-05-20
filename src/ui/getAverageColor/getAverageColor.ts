c71eabc804277a1dd24136078fa3880c5eaa5681
soujvnunes <soujvnunes@gmail.com>
2023-05-20 20:58:25 -0300

    feat: enhance getAverageColor

--- src/ui/getAverageColor/getAverageColor.ts
+++ src/ui/getAverageColor/getAverageColor.ts
@@ -1,19 +1,26 @@
+let rgb = [48, 51, 190];
+
 export default function getAverageColor(src: string) {
   const context = document.createElement('canvas').getContext?.('2d', {
     willReadFrequently: true
   });
   const image = new Image();
 
+  if (!context || !src) return rgb;
+
   image.crossOrigin = 'Anonymous';
   image.width = 64;
   image.height = 64;
   image.src = src;
-  context?.drawImage(image, 0, 0, 1, 64);
+  context.drawImage(image, 0, 0, 1, 64);
 
-  const imageData = context?.getImageData(0, 0, 1, 1);
+  try {
+    rgb = Array.from(
+      context.getImageData(0, 0, 1, 1).data.filter((_, index) => index <= 2)
+    );
+  } catch (error) {
+    return rgb;
+  }
 
-  if (imageData?.data)
-    return Array.from(imageData?.data.filter((_, index) => index <= 2));
-
-  return [48, 51, 190];
+  return rgb;
 }
