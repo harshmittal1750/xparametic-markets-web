bdb0e801b128dba11daa3b114d07d6f7677b5758
soujvnunes <soujvnunes@gmail.com>
2023-05-22 12:41:43 -0300

    feat: prefer returning a promise from getAverageColor

--- src/ui/getAverageColor/getAverageColor.ts
+++ src/ui/getAverageColor/getAverageColor.ts
@@ -1,26 +1,41 @@
-let rgb = [48, 51, 190];
+function getImage(src: string, options: Record<'width' | 'height', number>) {
+  return new Promise<HTMLImageElement>((resolve, reject) => {
+    const image = new Image();
 
-export default function getAverageColor(src: string) {
+    image.crossOrigin = 'Anonymous';
+    image.onload = () => resolve(image);
+    image.onerror = reject;
+    image.src = src;
+    image.width = options.width;
+    image.height = options.height;
+  });
+}
+
+/**
+ * Returns an RGB color array-like `[RRR, GGG, BBB]` from a given `src` image.
+ * @param src Source link image.
+ * @returns {number[]}
+ */
+export default async function getAverageColor(src: string) {
   const context = document.createElement('canvas').getContext?.('2d', {
     willReadFrequently: true
   });
-  const image = new Image();
+  const rgb = [48, 51, 190];
 
   if (!context || !src) return rgb;
 
-  image.crossOrigin = 'Anonymous';
-  image.width = 64;
-  image.height = 64;
-  image.src = src;
-  context.drawImage(image, 0, 0, 1, 64);
-
   try {
-    rgb = Array.from(
+    const image = await getImage(src, {
+      width: 64,
+      height: 64
+    });
+
+    context.drawImage(image, 0, 0, 1, 64);
+
+    return Array.from(
       context.getImageData(0, 0, 1, 1).data.filter((_, index) => index <= 2)
     );
   } catch (error) {
     return rgb;
   }
-
-  return rgb;
 }
