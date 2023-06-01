503932200f9f83ab45de74afb6c4692f7a8b3b05
Wellington Matheus <w.matheus@outlook.com>
2023-06-01 11:04:28 -0300

    feat(Leaderboard): create `LeaderboardMarkets`

---
+++ src/pages/Leaderboard/LeaderboardMarkets.tsx
@@ -0,0 +1,67 @@
+import { Link } from 'react-router-dom';
+
+import cn from 'classnames';
+import isEmpty from 'lodash/isEmpty';
+import { Tournament } from 'types/tournament';
+
+import { AlertMini } from 'components';
+
+import styles from './LeaderboardMarkets.module.scss';
+
+type LeaderboardMarketsProps = {
+  data?: Tournament['markets'];
+  isLoading: boolean;
+  emptyDataDescription?: string;
+};
+
+function LeaderboardMarkets({
+  data,
+  isLoading,
+  emptyDataDescription = 'No data to show.'
+}: LeaderboardMarketsProps) {
+  const isEmptyData = isEmpty(data);
+
+  return (
+    <div className="pm-c-leaderboard-stats bg-3 border-radius-medium border-solid border-1">
+      <h2 className="body semibold text-1">Markets</h2>
+      {isLoading ? (
+        <div className="flex-row justify-center align-center width-full padding-y-5 padding-x-4">
+          <span className="spinner--primary" />
+        </div>
+      ) : null}
+      {!isLoading && isEmptyData ? (
+        <AlertMini
+          style={{ border: 'none' }}
+          styles="outline"
+          variant="information"
+          description={emptyDataDescription}
+        />
+      ) : null}
+      {!isLoading && !isEmptyData ? (
+        <ul className={styles.root}>
+          {data?.map(market => (
+            <li key={market.slug}>
+              <Link className={styles.market} to={`/markets/${market.slug}`}>
+                <img
+                  className={styles.marketImage}
+                  src={market.imageUrl}
+                  alt={market.title}
+                />
+                <p
+                  className={cn(
+                    styles.marketTitle,
+                    'caption semibold text-2 text-1-on-hover'
+                  )}
+                >
+                  {market.title}
+                </p>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      ) : null}
+    </div>
+  );
+}
+
+export default LeaderboardMarkets;
