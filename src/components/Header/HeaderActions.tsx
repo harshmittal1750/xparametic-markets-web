09458295ba14593fff7f0b3190a380397da282da
soujvnunes <soujvnunes@gmail.com>
2023-06-15 20:44:28 -0300

    feat: adjust header actions flow

--- src/components/Header/HeaderActions.tsx
+++ src/components/Header/HeaderActions.tsx
@@ -14,7 +14,9 @@ import { useAppSelector, usePortal } from 'hooks';
 import headerClasses from './Header.module.scss';
 import headerActionsClasses from './HeaderActions.module.scss';
 
-function HeaderActionsWrapper(props: React.PropsWithChildren<{}>) {
+function HeaderActionsWrapper(
+  props: React.PropsWithChildren<Record<string, unknown>>
+) {
   const Portal = usePortal({
     root: document.body
   });
@@ -25,29 +27,40 @@ function HeaderActionsWrapper(props: React.PropsWithChildren<{}>) {
 
   return <Portal {...props} />;
 }
+function HeaderActionsGroup(
+  props: React.PropsWithChildren<Record<string, unknown>>
+) {
+  return <div className={headerActionsClasses.actionsGroup} {...props} />;
+}
 export default function HeaderActions() {
   const isLoggedIn = useAppSelector(state => state.polkamarkets.isLoggedIn);
   const { isDesktop } = useTheme().device;
   const Root = isDesktop ? Fragment : HeaderActionsWrapper;
   const Wrapper = isDesktop ? 'div' : Container;
   const HeaderActionsIntegration = features.fantasy.enabled ? Profile : Wallet;
+  const HeaderActionsGroupComponent = features.fantasy.enabled
+    ? Fragment
+    : HeaderActionsGroup;
 
   return (
     <Root>
       <Wrapper
         className={cn(headerActionsClasses.root, {
-          [headerClasses.container]: !isDesktop
+          [headerClasses.container]: !isDesktop,
+          [headerActionsClasses.reverse]: features.fantasy.enabled
         })}
       >
+        <HeaderActionsGroupComponent>
+          {ui.layout.header.networkSelector.enabled && isDesktop && (
+            <NetworkSelector
+              size="sm"
+              responsive
+              className={headerActionsClasses.network}
+            />
+          )}
+          <HeaderActionsIntegration isLoggedIn={isLoggedIn} />
+        </HeaderActionsGroupComponent>
         <ThemeSelector />
-        {ui.layout.header.networkSelector.enabled && isDesktop && (
-          <NetworkSelector
-            size="sm"
-            responsive
-            className={headerActionsClasses.network}
-          />
-        )}
-        <HeaderActionsIntegration isLoggedIn={isLoggedIn} />
       </Wrapper>
     </Root>
   );
