09a1972ae9f3c40618a5225f4a4fed63f0447097
Wellington Matheus <w.matheus@outlook.com>
2023-06-15 21:26:50 -0300

    fix(TradeActions): remove unused `useCallback` import

--- src/components/Trade/TradeActions.tsx
+++ src/components/Trade/TradeActions.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { changeOutcomeData, changeData } from 'redux/ducks/market';
 import { changeMarketOutcomeData, changeMarketData } from 'redux/ducks/markets';
