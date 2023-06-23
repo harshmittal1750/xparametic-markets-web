bdad3192bd758163be6fb27d4051ec05d6336cce
soujvnunes <soujvnunes@gmail.com>
2023-06-23 11:26:16 -0300

    feat: Display Avatar before username

--- src/pages/Profile/ProfileSummary.tsx
+++ src/pages/Profile/ProfileSummary.tsx
@@ -1,19 +1,40 @@
+import { Fragment } from 'react';
+
+import { features } from 'config';
 import { fromTimestampToCustomFormatDate } from 'helpers/date';
-import { Skeleton } from 'ui';
+import { useGetLeaderboardByAddressQuery } from 'services/Polkamarkets';
+import { Avatar, Skeleton } from 'ui';
 
 import { ShareIcon } from 'assets/icons';
 
 import { AlertMini, Tooltip, Feature } from 'components';
 import { Text } from 'components/new';
 
+import profileClasses from './Profile.module.scss';
 import type { ProfileSummaryProps } from './types';
 
+function ProfileSummaryGroup(
+  props: React.PropsWithChildren<Record<string, unknown>>
+) {
+  return <div className={profileClasses.summaryGroup} {...props} />;
+}
 export default function ProfileSummary({
   address,
   isLoading,
   data,
   network
 }: ProfileSummaryProps) {
+  const leaderboard = useGetLeaderboardByAddressQuery({
+    address,
+    networkId: network.id,
+    timeframe: '1w'
+  });
+  const hasAvatar = leaderboard.data?.userImageUrl && features.fantasy.enabled;
+  const ProfileSummaryGroupComponent = hasAvatar
+    ? ProfileSummaryGroup
+    : Fragment;
+  const ProfileSummaryInfoComponent = hasAvatar ? 'div' : Fragment;
+
   return (
     <div className="pm-p-profile-summary__details">
       {(() => {
@@ -36,52 +57,62 @@ export default function ProfileSummary({
             />
           );
         return (
-          <>
-            <Text
-              className="pm-p-profile-summary__address"
-              as="h2"
-              fontSize="heading-2"
-              fontWeight="bold"
-              color="1"
-            >
-              {address}
-              <Feature name="regular">
-                <Tooltip position="bottom-start" text="View on Explorer">
-                  <a
-                    target="_blank"
-                    href={`${network.explorerURL}/address/${address}`}
-                    rel="noreferrer"
-                  >
-                    <ShareIcon className="pm-p-profile-summary__explorer-icon" />
-                  </a>
-                </Tooltip>
-              </Feature>
-            </Text>
-            <Text
-              fontSize="body-4"
-              fontWeight="semibold"
-              color="3"
-              transform="uppercase"
-              className="pm-p-profile-summary__history"
-            >
-              {!!data?.firstPositionAt && (
-                <>
-                  First prediction:{' '}
-                  <span className="pm-p-profile-summary__history-accent">
-                    {fromTimestampToCustomFormatDate(
-                      data.firstPositionAt * 1000,
-                      'MMMM DD, YYYY'
-                    )}
-                  </span>
-                  <span className="pm-c-divider--circle" />
-                </>
-              )}
-              Total predictions:{' '}
-              <span className="pm-p-profile-summary__history-accent">
-                {data.totalPositions}
-              </span>
-            </Text>
-          </>
+          <ProfileSummaryGroupComponent>
+            {leaderboard.data?.userImageUrl && (
+              <Avatar
+                $radius="lg"
+                $size="md"
+                alt="avatar"
+                src={leaderboard.data?.userImageUrl}
+              />
+            )}
+            <ProfileSummaryInfoComponent>
+              <Text
+                className="pm-p-profile-summary__address"
+                as="h2"
+                fontSize="heading-2"
+                fontWeight="bold"
+                color="1"
+              >
+                {leaderboard.data?.username || address}
+                <Feature name="regular">
+                  <Tooltip position="bottom-start" text="View on Explorer">
+                    <a
+                      target="_blank"
+                      href={`${network.explorerURL}/address/${address}`}
+                      rel="noreferrer"
+                    >
+                      <ShareIcon className="pm-p-profile-summary__explorer-icon" />
+                    </a>
+                  </Tooltip>
+                </Feature>
+              </Text>
+              <Text
+                fontSize="body-4"
+                fontWeight="semibold"
+                color="3"
+                transform="uppercase"
+                className="pm-p-profile-summary__history"
+              >
+                {!!data?.firstPositionAt && (
+                  <>
+                    First prediction:{' '}
+                    <span className="pm-p-profile-summary__history-accent">
+                      {fromTimestampToCustomFormatDate(
+                        data.firstPositionAt * 1000,
+                        'MMMM DD, YYYY'
+                      )}
+                    </span>
+                    <span className="pm-c-divider--circle" />
+                  </>
+                )}
+                Total predictions:{' '}
+                <span className="pm-p-profile-summary__history-accent">
+                  {data.totalPositions}
+                </span>
+              </Text>
+            </ProfileSummaryInfoComponent>
+          </ProfileSummaryGroupComponent>
         );
       })()}
     </div>
