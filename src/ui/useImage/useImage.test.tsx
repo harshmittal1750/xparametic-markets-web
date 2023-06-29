0a2b75160683a305aad98f7e983713aaa85266fc
soujvnunes <soujvnunes@gmail.com>
2023-06-29 10:49:41 -0300

    test: simplify tests

--- src/ui/useImage/useImage.test.tsx
+++ src/ui/useImage/useImage.test.tsx
@@ -3,17 +3,12 @@ import { renderHook } from '@testing-library/react-hooks';
 
 import useImage from './useImage';
 
-it('returns OK state', () => {
-  const [state, ...props] = renderHook(() => useImage()).result.current;
+it('returns states', () => {
+  const [state, imageProps] = renderHook(() => useImage()).result.current;
 
   expect(state).toMatch('load');
-  render(<img src="" alt="test" {...props} />);
+  render(<img src="" alt="test" {...imageProps} />);
   expect(state).toMatch('ok');
-});
-it('returns error state', () => {
-  const [state, ...props] = renderHook(() => useImage()).result.current;
-
-  render(<img src="" alt="test" {...props} />);
   fireEvent.error(screen.getByAltText('test'));
   expect(state).toMatch('error');
 });
