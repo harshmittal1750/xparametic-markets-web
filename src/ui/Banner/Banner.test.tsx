24e9cd1c06440303931e39092a76e7625bdb6969
soujvnunes <soujvnunes@gmail.com>
2023-07-10 09:48:12 -0300

    test: adjust Banner

--- src/ui/Banner/Banner.test.tsx
+++ src/ui/Banner/Banner.test.tsx
@@ -2,14 +2,10 @@ import { render, screen } from '@testing-library/react';
 
 import Banner from './Banner';
 
-test('renders role', () => {
+test('renders roles', () => {
   render(<Banner />);
 
   expect(screen.getByRole('banner')).toBeInTheDocument();
-});
-test('renders hide button', () => {
-  render(<Banner />);
-
   expect(
     screen.getByRole('button', {
       name: 'Close'
