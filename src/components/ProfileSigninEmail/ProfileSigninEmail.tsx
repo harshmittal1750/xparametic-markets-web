d5030a290bccd23cc6dfc25ee62212f6accc2010
soujvnunes <soujvnunes@gmail.com>
2023-07-28 09:00:24 -0300

    feat: adjust email sign in

--- src/components/ProfileSigninEmail/ProfileSigninEmail.tsx
+++ src/components/ProfileSigninEmail/ProfileSigninEmail.tsx
@@ -1,7 +1,6 @@
 import { useCallback, useState } from 'react';
 
 import cn from 'classnames';
-import { Divider } from 'ui';
 
 import { Button } from 'components/Button';
 import type { ButtonProps } from 'components/Button';
@@ -13,39 +12,29 @@ export default function ProfileSigninEmail({
   ...props
 }: Pick<ButtonProps, 'onClick' | 'name' | 'children' | 'className'>) {
   const [email, setEmail] = useState('');
+  const handleEmail = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) =>
+      setEmail(event.target.value),
+    []
+  );
 
   return (
     <>
-      <Divider enableGutters />
-      <form
-        onSubmit={useCallback(
-          (event: React.SyntheticEvent) => event.preventDefault(),
-          []
-        )}
-      >
-        <label htmlFor="email" className="pm-c-input__group">
-          <input
-            className="pm-c-input--default"
-            id="email"
-            value={email}
-            placeholder="Write your email here"
-            onChange={useCallback(
-              (event: React.ChangeEvent<HTMLInputElement>) =>
-                setEmail(event.target.value),
-              []
-            )}
-          />
-        </label>
-        <Button
-          type="submit"
-          className={cn(profileSigninEmailClasses.email, className)}
-          color="default"
-          size="sm"
-          disabled={!email}
-          data-email={email}
-          {...props}
+      <label htmlFor="profile-signin-email" className="pm-c-input__group">
+        <input
+          type="email"
+          id="profile-signin-email"
+          className="pm-c-input--default"
+          placeholder="Write your email here"
+          onChange={handleEmail}
         />
-      </form>
+      </label>
+      <Button
+        disabled={!email}
+        data-email={email}
+        className={cn(profileSigninEmailClasses.email, className)}
+        {...props}
+      />
     </>
   );
 }
