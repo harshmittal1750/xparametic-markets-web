dd3cb33a5872d1c2a6ba21c6a96825e77465d06e
soujvnunes <soujvnunes@gmail.com>
2023-07-29 21:10:12 -0300

    feat: prefer submit email opt

--- src/components/ProfileSigninEmail/ProfileSigninEmail.tsx
+++ src/components/ProfileSigninEmail/ProfileSigninEmail.tsx
@@ -1,41 +1,30 @@
-import { useCallback, useState } from 'react';
-
 import cn from 'classnames';
 
 import { Button } from 'components/Button';
-import type { ButtonProps } from 'components/Button';
+import Icon from 'components/Icon';
 
 import profileSigninEmailClasses from './ProfileSigninEmail.module.scss';
 
-export default function ProfileSigninEmail({
-  className,
-  ...props
-}: Pick<ButtonProps, 'onClick' | 'name' | 'children' | 'className'>) {
-  const [email, setEmail] = useState('');
-  const handleEmail = useCallback(
-    (event: React.ChangeEvent<HTMLInputElement>) =>
-      setEmail(event.target.value),
-    []
-  );
-
+export default function ProfileSigninEmail(
+  props: Pick<React.ComponentPropsWithoutRef<'form'>, 'onSubmit'>
+) {
   return (
-    <>
+    <form {...props}>
       <label htmlFor="profile-signin-email" className="pm-c-input__group">
         <input
           type="email"
           id="profile-signin-email"
           className="pm-c-input--default"
-          placeholder="Write your email here"
-          onChange={handleEmail}
+          placeholder="example@mail.com"
         />
         <Button
-          disabled={!email}
-          data-email={email}
-          className={cn(profileSigninEmailClasses.email, className)}
-          {...props}
+          type="submit"
           variant="normal"
-        />
+          className={cn(profileSigninEmailClasses.email)}
+        >
+          <Icon size="lg" name="LogIn" />
+        </Button>
       </label>
-    </>
+    </form>
   );
 }
