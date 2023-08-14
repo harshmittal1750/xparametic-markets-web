1ced060c6724f3b5a8caa0c4df61d5109b476086
soujvnunes <soujvnunes@gmail.com>
2023-08-14 12:37:36 -0300

    feat: adjust email UI

--- src/components/ProfileSigninEmail/ProfileSigninEmail.tsx
+++ src/components/ProfileSigninEmail/ProfileSigninEmail.tsx
@@ -9,27 +9,20 @@ export default function ProfileSigninEmail(
   props: Pick<React.ComponentPropsWithoutRef<'form'>, 'onSubmit'>
 ) {
   return (
-    <form {...props}>
-      <label
-        htmlFor="profile-signin-email"
-        className="pm-c-input__group"
-        style={{ flexDirection: 'row', gap: 0 }}
+    <form className={profileSigninEmailClasses.root} {...props}>
+      <input
+        type="email"
+        className={profileSigninEmailClasses.input}
+        placeholder="Enter your e-mail"
+      />
+      <Button
+        type="submit"
+        variant="normal"
+        size="xs"
+        className={cn(profileSigninEmailClasses.email)}
       >
-        <input
-          type="email"
-          id="profile-signin-email"
-          className="pm-c-input--default"
-          placeholder="example@mail.com"
-          style={{ width: '100%' }}
-        />
-        <Button
-          type="submit"
-          variant="normal"
-          className={cn(profileSigninEmailClasses.email)}
-        >
-          <Icon size="lg" name="LogIn" />
-        </Button>
-      </label>
+        <Icon size="lg" name="LogIn" />
+      </Button>
     </form>
   );
 }
