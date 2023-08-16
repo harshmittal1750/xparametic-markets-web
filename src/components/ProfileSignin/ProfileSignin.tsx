140a6ee96f99a909a1b4ec97ca4140715f191c9b
soujvnunes <soujvnunes@gmail.com>
2023-08-16 20:09:30 -0300

    feat: adjust single provider email login

--- src/components/ProfileSignin/ProfileSignin.tsx
+++ src/components/ProfileSignin/ProfileSignin.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 import classNames from 'classnames';
 import { ui } from 'config';
@@ -22,68 +22,22 @@ import { useAppDispatch, usePolkamarketsService } from 'hooks';
 
 import profileSigninClasses from './ProfileSignin.module.scss';
 
-function getProviderCns(provider: Providers) {
-  return classNames(
-    profileSigninClasses.provider,
-    profileSigninClasses.social,
-    {
-      [profileSigninClasses.socialGoogle]: provider === 'Google',
-      [profileSigninClasses.socialFacebook]: provider === 'Facebook',
-      [profileSigninClasses.socialDiscord]: provider === 'Discord',
-      [profileSigninClasses.socialTwitter]: provider === 'Twitter',
-      [profileSigninClasses.socialMetaMask]: provider === 'MetaMask'
-    }
-  );
-}
-function getProviderChild(provider: Providers, isLoading: boolean) {
-  return isLoading ? (
-    <Spinner $size="md" />
-  ) : (
-    <Icon size="lg" name={provider === 'Email' ? 'LogIn' : provider} />
-  );
-}
+const hasSingleProvider = ui.socialLogin.providers.length === 1;
+const singleProviderName = ui.socialLogin.providers[0];
+
 export default function ProfileSignin() {
   const dispatch = useAppDispatch();
   const polkamarketsService = usePolkamarketsService();
   const [show, setShow] = useState(false);
   const [load, setLoad] = useState<Providers | ''>('');
-  const hasSingleProvider = ui.socialLogin.providers.length === 1;
-  const singleProviderName = ui.socialLogin.providers[0];
+  const handleClick = useCallback(
+    async (event: React.MouseEvent<HTMLButtonElement>) => {
+      const name = event.currentTarget.name as Exclude<Providers, 'Email'>;
 
-  function handleShow() {
-    setShow(true);
-  }
-  function handleHide() {
-    setShow(false);
-  }
-  async function handleClick(event: React.MouseEvent<HTMLButtonElement>) {
-    const name = event.currentTarget.name as Exclude<Providers, 'Email'>;
-
-    try {
-      setLoad(name);
-
-      const success = await polkamarketsService[`socialLogin${name}`]();
-
-      if (success) {
-        const { login } = await import('redux/ducks/polkamarkets');
-
-        dispatch(login(polkamarketsService));
-      }
-    } finally {
-      setLoad('');
-      setShow(false);
-    }
-  }
-  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-
-    if (event.target[0].value) {
       try {
-        setLoad('Email');
+        setLoad(name);
 
-        const success = await polkamarketsService.socialLoginEmail(
-          event.target[0].value
-        );
+        const success = await polkamarketsService[`socialLogin${name}`]();
 
         if (success) {
           const { login } = await import('redux/ducks/polkamarkets');
@@ -94,7 +48,121 @@ export default function ProfileSignin() {
         setLoad('');
         setShow(false);
       }
-    }
+    },
+    [dispatch, polkamarketsService]
+  );
+  const handleSubmit = useCallback(
+    async (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+
+      if (event.target[0].value) {
+        try {
+          setLoad('Email');
+
+          const success = await polkamarketsService.socialLoginEmail(
+            event.target[0].value
+          );
+
+          if (success) {
+            const { login } = await import('redux/ducks/polkamarkets');
+
+            dispatch(login(polkamarketsService));
+          }
+        } finally {
+          setLoad('');
+          setShow(false);
+        }
+      }
+    },
+    [dispatch, polkamarketsService]
+  );
+  const renderProviders = useCallback(
+    provider => {
+      const isLoading = !!load && load === provider;
+      const isDisabled = !!load && load !== provider;
+      const child = (
+        <>
+          {provider === 'Email' ? (
+            ''
+          ) : (
+            <>
+              {hasSingleProvider && (
+                <>
+                  <Icon
+                    name="LogIn"
+                    size="lg"
+                    className={profileSigninClasses.signinIcon}
+                  />
+                  Login with{' '}
+                </>
+              )}
+              {provider}
+            </>
+          )}
+          {isLoading ? (
+            <Spinner $size="md" />
+          ) : (
+            <Icon size="lg" name={provider === 'Email' ? 'LogIn' : provider} />
+          )}
+        </>
+      );
+      const className = classNames(
+        profileSigninClasses.provider,
+        profileSigninClasses.social,
+        {
+          [profileSigninClasses.socialGoogle]: provider === 'Google',
+          [profileSigninClasses.socialFacebook]: provider === 'Facebook',
+          [profileSigninClasses.socialDiscord]: provider === 'Discord',
+          [profileSigninClasses.socialTwitter]: provider === 'Twitter',
+          [profileSigninClasses.socialMetaMask]: provider === 'MetaMask'
+        }
+      );
+
+      if (provider === 'Email')
+        return (
+          <ProfileSigninEmail
+            key={provider}
+            disabled={isDisabled}
+            onSubmit={handleSubmit}
+          >
+            {child}
+          </ProfileSigninEmail>
+        );
+
+      if (provider === 'MetaMask')
+        return (
+          <ConnectMetamask
+            key={provider}
+            className={className}
+            disabled={isDisabled}
+          >
+            {child}
+          </ConnectMetamask>
+        );
+
+      return (
+        <Button
+          variant="outline"
+          color="default"
+          size="sm"
+          key={provider}
+          name={provider}
+          className={className}
+          onClick={handleClick}
+          disabled={isDisabled}
+        >
+          {child}
+        </Button>
+      );
+    },
+    [handleClick, handleSubmit, load]
+  );
+
+  function handleShow() {
+    setShow(true);
+  }
+  function handleHide() {
+    setShow(false);
   }
 
   return (
@@ -128,82 +196,29 @@ export default function ProfileSignin() {
               Select one of the following to continue.
             </Text>
             <div className={profileSigninClasses.providers}>
-              {ui.socialLogin.providers.map(provider => {
-                const isLoading = !!load && load === provider;
-                const isDisabled = !!load && load !== provider;
-                const child = (
-                  <>
-                    {provider === 'Email' ? '' : provider}
-                    {getProviderChild(provider, isLoading)}
-                  </>
-                );
-                const className = getProviderCns(provider);
-
-                if (provider === 'Email')
-                  return (
-                    <ProfileSigninEmail
-                      key={provider}
-                      disabled={isDisabled}
-                      onSubmit={handleSubmit}
-                    >
-                      {child}
-                    </ProfileSigninEmail>
-                  );
-
-                if (provider === 'MetaMask')
-                  return (
-                    <ConnectMetamask
-                      key={provider}
-                      className={className}
-                      disabled={isDisabled}
-                    >
-                      {child}
-                    </ConnectMetamask>
-                  );
-
-                return (
-                  <Button
-                    variant="outline"
-                    color="default"
-                    size="sm"
-                    key={provider}
-                    name={provider}
-                    className={className}
-                    onClick={handleClick}
-                    disabled={isDisabled}
-                  >
-                    {child}
-                  </Button>
-                );
-              })}
+              {ui.socialLogin.providers.map(renderProviders)}
             </div>
           </ModalSection>
         </ModalContent>
       </Modal>
-      <Button
-        variant="ghost"
-        color="default"
-        size="sm"
-        name={hasSingleProvider ? singleProviderName : undefined}
-        onClick={hasSingleProvider ? handleClick : handleShow}
-        className={
-          hasSingleProvider
-            ? getProviderCns(singleProviderName)
-            : profileSigninClasses.signin
-        }
-      >
-        <Icon
-          name="LogIn"
-          size="lg"
-          className={profileSigninClasses.signinIcon}
-        />
-        Login
-        {hasSingleProvider && ` with ${singleProviderName}`}
-        {getProviderChild(
-          singleProviderName,
-          !!load && load === singleProviderName
-        )}
-      </Button>
+      {hasSingleProvider ? (
+        renderProviders(singleProviderName)
+      ) : (
+        <Button
+          variant="ghost"
+          color="default"
+          size="sm"
+          onClick={handleShow}
+          className={profileSigninClasses.signin}
+        >
+          <Icon
+            name="LogIn"
+            size="lg"
+            className={profileSigninClasses.signinIcon}
+          />
+          Login
+        </Button>
+      )}
     </>
   );
 }
