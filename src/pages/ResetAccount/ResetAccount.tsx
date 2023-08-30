c2ab0b4955f16375662df07599a41b595bffcd8d
Wellington Matheus <w.matheus@outlook.com>
2023-08-30 13:46:52 -0300

    feat(ResetAccount): fetch portfolio and add `ResetAccountMarkets`

--- src/pages/ResetAccount/ResetAccount.tsx
+++ src/pages/ResetAccount/ResetAccount.tsx
@@ -1,29 +1,54 @@
 import { useCallback, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
+import { getPortfolio } from 'redux/ducks/portfolio';
 import { Container } from 'ui';
 
 import { Button } from 'components';
 
-import { useAppSelector } from 'hooks';
+import { useAppDispatch, useAppSelector, useNetwork } from 'hooks';
 
 import styles from './ResetAccount.module.scss';
+import ResetAccountMarkets from './ResetAccountMarkets';
 
 function ResetAccount() {
+  const dispatch = useAppDispatch();
   const history = useHistory();
-  const walletConnected = useAppSelector(
-    state => state.polkamarkets.isLoggedIn
+  const { network } = useNetwork();
+
+  const isLoggedIn = useAppSelector(state => state.polkamarkets.isLoggedIn);
+  const isLoadingLogin = useAppSelector(
+    state => state.polkamarkets.isLoading.login
   );
+  const ethAddress = useAppSelector(state => state.polkamarkets.ethAddress);
 
   const redirectToHome = useCallback(() => {
     history.push('/');
   }, [history]);
 
   useEffect(() => {
-    if (!walletConnected) {
+    if (!isLoadingLogin && !isLoggedIn) {
       redirectToHome();
     }
-  }, [redirectToHome, walletConnected]);
+  }, [isLoadingLogin, isLoggedIn, redirectToHome]);
+
+  useEffect(() => {
+    async function fetchPortfolio() {
+      dispatch(getPortfolio(ethAddress, network.id));
+    }
+
+    if (!isLoadingLogin && isLoggedIn) {
+      fetchPortfolio();
+    }
+  }, [dispatch, ethAddress, isLoadingLogin, isLoggedIn, network.id]);
+
+  if (isLoadingLogin) {
+    return (
+      <div className="flex-row justify-center align-center width-full padding-y-5 padding-x-4">
+        <span className="spinner--primary" />
+      </div>
+    );
+  }
 
   return (
     <Container className="flex-column justify-start align-start gap-5 width-full max-width-screen-xl">
@@ -39,6 +64,7 @@ function ResetAccount() {
           </Button>
         </div>
       </div>
+      <ResetAccountMarkets />
     </Container>
   );
 }
