6fe5d8767703246a863d8f96cb99785d95a205c7
Wellington Matheus <w.matheus@outlook.com>
2023-09-25 18:09:50 -0300

    feat(Pages): create `/whitelist`

---
+++ src/pages/Whitelist/Whitelist.tsx
@@ -0,0 +1,76 @@
+import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import { Spinner } from 'ui';
+
+import { PCLLogo } from 'assets/icons';
+
+import {
+  Link,
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalSection,
+  ModalSectionText,
+  Text
+} from 'components';
+
+import { useWhitelist } from 'contexts/whitelist';
+
+import styles from './Whitelist.module.scss';
+
+export default function Whitelist() {
+  const history = useHistory();
+  const { isLoading, email, isWhitelisted } = useWhitelist();
+
+  useEffect(() => {
+    if (!isLoading && isWhitelisted) {
+      history.push('/');
+    }
+  }, [history, isLoading, isWhitelisted]);
+
+  return (
+    <div className={styles.root}>
+      <Modal show centered size="sm">
+        <ModalContent className={styles.content}>
+          <ModalHeader>
+            <PCLLogo />
+            <Text
+              as="h5"
+              scale="heading"
+              fontWeight="bold"
+              className={styles.contentTitle}
+            >
+              {isLoading
+                ? `Please wait while we verify your address.`
+                : `Your
+              address is not whitelisted in our application.`}
+            </Text>
+          </ModalHeader>
+          <ModalSection>
+            {isLoading ? (
+              <Spinner />
+            ) : (
+              <>
+                <ModalSectionText>
+                  {`The email address ${email} is not whitelisted in our application`}
+                </ModalSectionText>
+                <ModalSectionText>
+                  For further information please contact us on{' '}
+                  <Link
+                    href="mailto:general@polkamarkets.com"
+                    title="general@polkamarkets.com"
+                    scale="caption"
+                    fontWeight="medium"
+                    className="text-gray"
+                    target="_blank"
+                  />
+                </ModalSectionText>
+              </>
+            )}
+          </ModalSection>
+        </ModalContent>
+      </Modal>
+    </div>
+  );
+}
