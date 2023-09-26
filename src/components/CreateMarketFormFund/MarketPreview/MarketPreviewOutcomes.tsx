7aa2a74a6b7319c0954c521bbf88a1ed5e024b77
Wellington Matheus <w.matheus@outlook.com>
2023-09-26 09:45:29 -0300

    feat(MarketPreviewOutcomes): add outcome images

--- src/components/CreateMarketFormFund/MarketPreview/MarketPreviewOutcomes.tsx
+++ src/components/CreateMarketFormFund/MarketPreview/MarketPreviewOutcomes.tsx
@@ -1,5 +1,5 @@
 import cn from 'classnames';
-import { useTheme } from 'ui';
+import { Avatar, useTheme } from 'ui';
 
 import type { Outcome } from '../../CreateMarketForm';
 import Text from '../../Text';
@@ -29,45 +29,55 @@ function MarketPreviewOutcomes({
         MarketPreviewOutcomesClasses.outcomes
       )}
     >
-      {on.map(outcome => (
-        <li key={outcome.id}>
-          <div
-            className={cn(
-              MarketPreviewOutcomesClasses.root,
-              'pm-c-market-outcomes__item--default'
-            )}
-          >
-            <div className={MarketPreviewOutcomesClasses.content}>
-              <div className="pm-c-market-outcomes__item-group--column">
-                <Text
-                  as="p"
-                  scale="caption"
-                  fontWeight="semibold"
-                  className="pm-c-market-outcomes__item-title"
-                >
-                  {outcome.name}
-                </Text>
-                <Text
-                  as="p"
-                  scale="tiny"
-                  fontWeight="semibold"
-                  className="pm-c-market-outcomes__item-odd notranslate"
-                >
+      {on.map(outcome => {
+        const image = outcome.image?.hash
+          ? `https://polkamarkets.infura-ipfs.io/ipfs/${outcome.image.hash}`
+          : undefined;
+        return (
+          <li key={outcome.id}>
+            <div
+              className={cn(
+                MarketPreviewOutcomesClasses.root,
+                'pm-c-market-outcomes__item--default'
+              )}
+              style={{
+                // @ts-expect-error No need to assert React.CSSProperties here
+                '--outcome-image': image && `url('${image}')`
+              }}
+            >
+              <div className={MarketPreviewOutcomesClasses.content}>
+                <div className="pm-c-market-outcomes__item-group--column">
                   <Text
-                    as="span"
-                    scale="tiny"
-                    fontWeight="bold"
-                    className="pm-c-market-outcomes__item-value"
+                    as="p"
+                    scale="caption"
+                    fontWeight="semibold"
+                    className="pm-c-market-outcomes__item-title"
                   >
-                    {`${(outcome.probability / 100).toFixed(3)} `}
+                    {outcome.name}
                   </Text>
-                  {ticker}
-                </Text>
+                  <Text
+                    as="p"
+                    scale="tiny"
+                    fontWeight="semibold"
+                    className="pm-c-market-outcomes__item-odd notranslate"
+                  >
+                    <Text
+                      as="span"
+                      scale="tiny"
+                      fontWeight="bold"
+                      className="pm-c-market-outcomes__item-value"
+                    >
+                      {`${(outcome.probability / 100).toFixed(3)} `}
+                    </Text>
+                    {ticker}
+                  </Text>
+                </div>
+                {image ? <Avatar $size="xs" $radius="xs" src={image} /> : null}
               </div>
             </div>
-          </div>
-        </li>
-      ))}
+          </li>
+        );
+      })}
       {off.length > 0 ? (
         <li>
           <div
