d5514377d5a08cda98dc6f7cbbcc101e44701291
soujvnunes <soujvnunes@gmail.com>
2023-10-04 19:53:03 -0300

    feat(MarketAvatar): allow custom [$radius]

--- src/components/MarketAvatar/MarketAvatar.tsx
+++ src/components/MarketAvatar/MarketAvatar.tsx
@@ -9,17 +9,17 @@ import Logos from 'components/Logos';
 import marketAvatarClasses from './MarketAvatar.module.scss';
 
 type MarketAvatarProps = Pick<Market, 'verified'> &
-  Pick<ImageProps, '$size'> &
+  Pick<ImageProps, '$size' | '$radius'> &
   Record<'imageUrl', string>;
 
 export default function MarketAvatar({
   imageUrl,
-  $size,
-  verified
+  verified,
+  ...props
 }: MarketAvatarProps) {
   return (
     <div className={marketAvatarClasses.root}>
-      <Image $radius="lg" alt="Market" $size={$size} src={imageUrl}>
+      <Image alt="Market" src={imageUrl} {...props}>
         <Logos size="md" standard="mono" />
       </Image>
       {verified && (
