4e430ecbed5354f7106c9877cb971d8328350d5e
soujvnunes <soujvnunes@gmail.com>
2023-10-09 12:35:29 -0300

    feat(TournamentsList): display Avatar only when `market.imageUrl` is present

--- src/pages/Tournaments/TournamentsList.tsx
+++ src/pages/Tournaments/TournamentsList.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 
 import cn from 'classnames';
+import { isNull } from 'lodash';
 import isEmpty from 'lodash/isEmpty';
 import { useGetTournamentsQuery } from 'services/Polkamarkets';
 import { Avatar } from 'ui';
@@ -39,12 +40,14 @@ function TournamentsList() {
               'bg-3': index % 2 === 0
             })}
           >
-            <Avatar
-              src={tournament.imageUrl || ''}
-              alt={tournament.title}
-              $size="xs"
-              $radius="xs"
-            />
+            {!isNull(tournament.imageUrl) && (
+              <Avatar
+                src={tournament.imageUrl}
+                alt={tournament.title}
+                $size="xs"
+                $radius="xs"
+              />
+            )}
             <div>
               <span className="body semibold text-2 text-1-on-hover">
                 {tournament.title}
